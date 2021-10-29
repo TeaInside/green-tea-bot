8ca980cd92c7a129950490f05e0a2a26e9550f64
Ammar Faizi <ammarfaizi2@gmail.com>
2021-10-29 14:16:14 +0700

    greentea/mysql: Fix runtests script
    Signed-off-by: Ammar Faizi <ammarfaizi2@gmail.com>

--- greentea/mysql/tests/query_fetch.cpp
+++ greentea/mysql/tests/query_fetch.cpp
@@ -1,49 +1,89 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*
+ * Copyright (C) 2021  Ammar Faizi <ammarfaizi2@gmail.com>
+ */
 
+#include <cassert>
+#include <cstdlib>
+#include <cstring>
 #include <iostream>
 
 #include "MySQL.hpp"
 
 
-static void do_test(void)
+static int test_fetch_001(mysql::MySQL *db)
 {
+	int ret, i;
 	MYSQL_ROW row;
 	int num_fields;
 	mysql::MySQLRes *res;
+
+	ret = db->query("SELECT 1, \"A\" UNION SELECT 2, \"B\" UNION SELECT 3, \"C\"");
+	assert(ret == 0);
+
+	res = db->storeResultRaw();
+	assert(res);
+
+	num_fields = res->numFields();
+	assert(num_fields == 2);
+
+	i = 0;
+	while ((row = res->fetchRow())) {
+		i++;
+		char str[32];
+
+		snprintf(str, sizeof(str), "%d", i);
+		assert(!strcmp(row[0], str));
+
+		snprintf(str, sizeof(str), "%c", 'A' + (i - 1));
+		assert(!strcmp(row[1], str));
+	}
+
+	assert(i == 3);
+	delete res;
+
+	return 0;
+}
+
+
+static int do_test(void)
+{
+	int ret = 0;
 	mysql::MySQL *db = nullptr;
-	const char *host = "127.0.0.1";
-	const char *user = "memcpy";
-	const char *passwd = "858869123qweASDzxc";
-	const char *dbname = "gw_telegram_new";
-	uint16_t port = 9999;
+	const char *host = getenv("TEST_MYSQL_HOST");
+	const char *user = getenv("TEST_MYSQL_USER");
+	const char *passwd = getenv("TEST_MYSQL_PASSWORD");
+	const char *dbname = getenv("TEST_MYSQL_DBNAME");
+	const char *port_str = getenv("TEST_MYSQL_PORT");
+	uint16_t port = (uint16_t)atoi(port_str ? port_str : "0");
+
+	assert(host);
+	assert(user);
+	assert(passwd);
+	assert(dbname);
 
 	try {
 		db = new mysql::MySQL(host, user, passwd, dbname);
 		db->setPort(port);
 		db->connect();
-		db->query("SELECT * FROM `gw_group_message_data` LIMIT 50");
-		res = db->storeResultRaw();
+
+		ret = test_fetch_001(db);
+		if (ret)
+			goto out;
+
 	} catch (const std::runtime_error& e) {
-		std::cout << e.what() << std::endl;
+		ret = 1;
+		std::cout << "Error: " << e.what() << std::endl;
 		goto out;
 	}
 
-	num_fields = res->numFields();
-	while ((row = res->fetchRow())) {
-		int i;
-
-		for (i = 0; i < num_fields; i++) {
-			printf("%s ", row[i] ? row[i] : "NULL");
-		}
-		printf("\n");
-	}
-
-	delete res;
 out:
 	delete db;
+	return ret;
 }
 
 
 int main(void)
 {
-	do_test();
+	return do_test();
 }
