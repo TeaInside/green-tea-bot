b2ef4ba3d7bcfd238e7de9a5c7fbb1ae7a4cb498
Ilham Muttaqien <ilham.muttaqien@students.amikom.ac.id>
2021-11-05 18:28:56 +0700

    web: Fix Login Page
    Signed-off-by: Ilham Muttaqien <ilham.muttaqien@students.amikom.ac.id>

--- web/green-tea/pages/index.js
+++ web/green-tea/pages/index.js
@@ -1,9 +1,14 @@
+import Head from "next/head";
 import Dashboard from "../components/Dashboard";
 import Sidebar from "../components/Sidebar";
 
 export default function Home() {
     return (
         <div className="flex">
+            <Head>
+                <title>GreenTea Dashboard</title>
+                <link rel="icon" href="/greentea.ico" />
+            </Head>
             <Sidebar />
             <Dashboard />
         </div>
--- web/green-tea/pages/login.js
+++ web/green-tea/pages/login.js
@@ -4,19 +4,20 @@ function login() {
     return (
         <div className="w-screen bg-cream h-screen pt-8">
             <Head>
-                <title>Login Admin</title>
+                <title>GreenTea Dashboard</title>
+                <link rel="icon" href="/greentea.ico" />
             </Head>
 
-            <main>
-                <div className="flex flex-col w-4/12 bg-white mx-auto px-12 py-12 h-[550px] space-y-5">
-                    <img
-                        className="h-[300px] w-[300px] mx-auto"
-                        src="greentea.svg"
-                        alt="logo"
-                    />
-                    <h1 className="text-gray-600 font-sans font-semibold text-[30px] text-center">
-                        Login to your account
-                    </h1>
+            <main className="flex flex-col w-4/12 bg-white mx-auto px-12 py-12  space-y-5">
+                <img
+                    className="w-[300px] mx-auto"
+                    src="greentea.svg"
+                    alt="logo"
+                />
+                <h1 className="text-gray-600 font-sans font-semibold text-[30px] text-center">
+                    Login to your account
+                </h1>
+                <form className="flex flex-col space-y-5" action="" method="">
                     <div className="flex flex-col">
                         <label
                             className="text-gray-500 text-[20px] font-sans"
@@ -53,7 +54,7 @@ function login() {
                     >
                         Login
                     </a>
-                </div>
+                </form>
             </main>
         </div>
     );
