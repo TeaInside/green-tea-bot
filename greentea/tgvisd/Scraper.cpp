TeaInside/green-tea-bot

commit 71174b375e52b6513f6800c7ff6cb4dfc6d1b08e

greetea/tgvisd: Implement scraper kworker
This is a multiple thread pool. The scraper-master will generate
task work and submit it to the thread pools. The thread pools will
be reaping and doing the task.

Signed-off-by: Ammar Faizi <ammarfaizi2@gmail.com>

greentea/tgvisd/Scraper.cpp
@@ -1,7 +1,7 @@
-// SPDX-License-Identifier: GPL-2.0
+// SPDX-License-Identifier: GPL-2.0-only
 /*
  * @author Ammar Faizi <ammarfaizi2@gmail.com> https://www.facebook.com/ammarfaizi2
- * @license GPL-2.0
+ * @license GPL-2.0-only
  * @package tgvisd
  *
  * Copyright (C) 2021  Ammar Faizi <ammarfaizi2@gmail.com>
@@ -12,10 +12,28 @@
 	#include <pthread.h>
 #endif
 
+#include <stack>
+#include <mutex>
+#include <queue>
+#include <chrono>
+#include <cstdio>
+#include <cstring>
 #include <cassert>
+#include <cstdlib>
+#include <unordered_map>
+#include <condition_variable>
 #include <tgvisd/Scraper.hpp>
 
+#include <mysql/MySQL.hpp>
+
+
 namespace tgvisd {
+
+
+using std::mutex;
+using std::unordered_map;
+using std::condition_variable;
+using namespace std::chrono_literals;
 
 
 Scraper::Scraper(Main *main, std::thread *threadPtr):
@@ -25,11 +43,8 @@ Scraper::Scraper(Main *main, std::thread *threadPtr):
 
 #if defined(__linux__)
 	pthread_t pt = threadPtr->native_handle();
-	pthread_setname_np(pt, "scraper");
+	pthread_setname_np(pt, "scraper-master");
 #endif
-
-	td_ = main->getTd();
-	assert(td_);
 }
 
 
@@ -39,77 +54,321 @@ Scraper::~Scraper(void)
 }
 
 
-void Scraper::run(void)
+class KWorker;
+class KWorkerPool;
+
+
+static void run_kworker(uint32_t kwrk_id, KWorkerPool *kwrk_pool);
+
+
+static void run_kworker_event_loop(KWorker *kwrk);
+
+
+struct task_work {
+	int64_t		chat_id;
+};
+
+
+struct task_work_list {
+	bool			is_used;
+	struct task_work	tw;
+};
+
+
+class KWorkerPool
 {
-	while (unlikely(!main_->isReady() && !main_->getStop())) {
-		pr_notice("In scraper (waiting for main thread to be ready)...");
-		sleep(1);
+public:
+	KWorker			*kworker_   = nullptr;
+	mutex			ftLock_;
+	condition_variable	ftCond_;
+	std::stack<uint16_t>	freeThreads_;
+
+private:
+	std::thread		**threads_  = nullptr;
+	mysql::MySQL		**dbPool_   = nullptr;
+	struct task_work_list	*tasks_     = nullptr;
+	uint16_t 		wrkNum_     = 0u;
+
+
+	inline void cleanUp(void)
+	{
+		uint16_t i;
+
+		if (threads_) {
+			for (i = 0; i < wrkNum_; i++) {
+				if (threads_[i]) {
+					threads_[i]->join();
+					delete threads_[i];
+					threads_[i] = nullptr;
+				}
+			}
+			free(threads_);
+			threads_ = nullptr;
+		}
+
+
+		if (dbPool_) {
+			for (i = 0; i < wrkNum_; i++) {
+				if (dbPool_[i]) {
+					delete dbPool_[i];
+					dbPool_[i] = nullptr;
+				}
+			}
+			free(dbPool_);
+			dbPool_ = nullptr;
+		}
+
+		if (tasks_) {
+			free(tasks_);
+			tasks_ = nullptr;
+		}
 	}
 
+
+public:
+	inline KWorkerPool(uint32_t wrkNum, KWorker *kworker):
+		kworker_(kworker),
+		wrkNum_(wrkNum)
+	{
+	}
+
+
+	inline KWorker *getKWorker(void)
+	{
+		return kworker_;
+	}
+
+
+	inline std::thread *getThread(uint16_t kwrk_id)
+	{
+		return threads_[kwrk_id];
+	}
+
+
+	inline void spawn(void)
+	{
+		uint16_t i;
+
+		threads_ = (std::thread **)calloc(wrkNum_, sizeof(*threads_));
+		if (unlikely(!threads_))
+			goto err_nomem;
+
+		dbPool_ = (mysql::MySQL **)calloc(wrkNum_, sizeof(*dbPool_));
+		if (unlikely(!dbPool_))
+			goto err_nomem;
+
+		tasks_ = (struct task_work_list *)calloc(wrkNum_, sizeof(*tasks_));
+		if (unlikely(!tasks_))
+			goto err_nomem;
+
+		for (i = 0; i < wrkNum_; i++) {
+			threads_[i] = new std::thread(run_kworker, i, this);
+			freeThreads_.push(i);
+		}
+
+		return;
+	err_nomem:
+		cleanUp();
+		throw std::bad_alloc();
+	}
+
+
+	inline struct task_work_list *getTW(uint16_t kwrk_id)
+	{
+		return &tasks_[kwrk_id];
+	}
+
+
+	inline int submitTaskWork(struct task_work *tw)
+	{
+		uint16_t kwrk_id;
+
+		ftLock_.lock();
+		if (freeThreads_.empty()) {
+			ftLock_.unlock();
+			return -EAGAIN;
+		}
+
+		kwrk_id = freeThreads_.top();
+		freeThreads_.pop();
+		ftLock_.unlock();
+
+		tasks_[kwrk_id].is_used = true;
+		tasks_[kwrk_id].tw = *tw;
+		ftCond_.notify_all();
+		return 0;
+	}
+
+
+	inline void putTaskWork(uint16_t kwrk_id)
+	{
+		ftLock_.lock();
+		tasks_[kwrk_id].is_used = false;
+		freeThreads_.push(kwrk_id);
+		ftLock_.unlock();
+
+		ftCond_.notify_all();
+	}
+
+
+	inline ~KWorkerPool(void)
+	{
+		cleanUp();
+	}
+};
+
+
+class KWorker
+{
+private:
+	tgvisd::Td::Td		*td_        = nullptr;
+	Main			*main_      = nullptr;
+	Scraper			*scraper_   = nullptr;
+	KWorkerPool		*kwrkPool_  = nullptr;
+
+public:
+	inline tgvisd::Td::Td *getTd(void)
+	{
+		return td_;
+	}
+
+
+	inline Main *getMain(void)
+	{
+		return main_;
+	}
+
+
+	inline Scraper *getScraper(void)
+	{
+		return scraper_;
+	}
+
+
+	inline KWorkerPool *getKWorkerPool(void)
+	{
+		return kwrkPool_;
+	}
+
+
+	inline KWorker(Scraper *scraper):
+		scraper_(scraper)
+	{
+		main_ = scraper->getMain();
+		td_   = main_->getTd();
+	}
+
+
+	inline bool kworkerShouldStop(void)
+	{
+		return unlikely(scraper_->getStop() || main_->getStop());
+	}
+
+
+	inline void run(void)
+	{
+		kwrkPool_ = new KWorkerPool(4, this);
+		kwrkPool_->spawn();
+		run_kworker_event_loop(this);
+		scraper_->doStop();
+		delete kwrkPool_;
+		kwrkPool_ = nullptr;
+	}
+};
+
+
+void Scraper::run(void)
+{
+	KWorker *kworker = nullptr;
+
 	try {
-
-		while (likely(!main_->getStop() && !stopScraper))
-			scraperEventLoop();
-
+		kworker = new KWorker(this);
+		kworker->run();
 	} catch (const std::runtime_error &e) {
 		pr_err("In scraper: std::runtime_error: %s", e.what());
 		main_->doStop();
+	} catch (const std::bad_alloc &e) {
+		pr_err("In scraper: Aiee... ENOMEM!");
+		main_->doStop();
 	}
+
+	if (kworker)
+		delete kworker;
 }
 
 
-td_api::object_ptr<td_api::chats> Scraper::getChats(
-		td_api::object_ptr<td_api::ChatList> &&chatList, int32_t limit)
+static void wait_for_pool_assignment(uint32_t kwrk_id, KWorkerPool *kwrk_pool)
 {
-	const uint32_t timeout = 150;
-	return td_->send_query_sync<td_api::getChats, td_api::chats>(
-		td_api::make_object<td_api::getChats>(std::move(chatList), limit),
-		timeout
-	);
+#if defined(__linux__)	
+	pthread_t pt;
+#endif
+	std::thread *current;
+	char buf[sizeof("scraper-wrk-xxxxxxx")];
+
+	do {
+		current = kwrk_pool->getThread(kwrk_id);
+		cpu_relax();
+	} while (unlikely(!current));
+
+#if defined(__linux__)
+	pt = current->native_handle();
+	snprintf(buf, sizeof(buf), "scraper-kwrk-%u", kwrk_id);
+	pthread_setname_np(pt, buf);
+#endif
 }
 
 
-td_api::object_ptr<td_api::chat> Scraper::getChat(int64_t chat_id)
+static void _run_kworker(uint32_t kwrk_id, KWorkerPool *kwrk_pool,
+			 std::unique_lock<mutex> &lk)
 {
-	const uint32_t timeout = 150;
-	return td_->send_query_sync<td_api::getChat, td_api::chat>(
-		td_api::make_object<td_api::getChat>(chat_id),
-		timeout
-	);
-}
+	struct task_work_list *twl;
 
+	lk.lock();
+	kwrk_pool->ftCond_.wait_for(lk, 1000ms);
+	lk.unlock();
 
-void Scraper::scraperEventLoop(void)
-{
-	int32_t i, total;
-
-	pr_debug("ChatScraper: Getting chatList...");
-	/*
-	 * @chats only contain list of chat_id(s) (array of integer here).
-	 */
-	auto chats = getChats(nullptr, 300);
-	total = chats->total_count_;
-	pr_debug("ChatScraper: Got %d chat ID(s)", total);
-	pr_debug("Enumerating chat_ids...");
-
-	for (i = 0; i < total; i++)
-		scrapeChat(chats->chat_ids_[i]);
-
-	sleep(3);
-}
-
-
-void Scraper::scrapeChat(int64_t chat_id)
-{
-	auto chat = getChat(chat_id);
-
-	if (unlikely(!chat))
+	twl = kwrk_pool->getTW(kwrk_id);
+	if (!twl->is_used)
 		return;
 
-	if (chat->type_->get_id() != td_api::chatTypeSupergroup::ID)
-		return;
+	printf("Scraping chat_id = %ld\n", twl->tw.chat_id);
+	kwrk_pool->putTaskWork(kwrk_id);
+}
 
-	pr_notice("Visiting %ld (%s)...", chat_id, chat->title_.c_str());
+
+static void run_kworker(uint32_t kwrk_id, KWorkerPool *kwrk_pool)
+{
+	KWorker *kwrk = kwrk_pool->kworker_;
+	std::unique_lock<mutex> lk(kwrk_pool->ftLock_, std::defer_lock);
+
+	wait_for_pool_assignment(kwrk_id, kwrk_pool);
+
+	while (!kwrk->kworkerShouldStop())
+		_run_kworker(kwrk_id, kwrk_pool, lk);
+}
+
+
+static void run_kworker_event_loop(KWorker *kwrk)
+{
+	int64_t i = 0;
+	struct task_work tw;
+	KWorkerPool *kwrk_pool;
+
+	kwrk_pool = kwrk->getKWorkerPool();
+	for (i = 0; i < 1000; i++) {
+		int ret;
+
+		tw.chat_id = i;
+	submit_again:
+		if (kwrk->kworkerShouldStop())
+			return;
+
+		ret = kwrk_pool->submitTaskWork(&tw);
+		if (ret == -EAGAIN) {
+			pr_notice("Threads are all busy...");
+			goto submit_again;
+		}
+	}
 }
 
 

greentea/tgvisd/CMakeLists.txt
@@ -36,6 +36,7 @@ target_compile_options(tgvisdtd PRIVATE
 	-Wno-unused-parameter
 	-fno-omit-frame-pointer
 	-fstack-protector-strong
+	-fsanitize=address
 )
 ##################################################################
 
@@ -52,6 +53,8 @@ target_compile_options(tgvisdtd PRIVATE
 
 set(
 	TGVISD_SOURCE
+	../mysql/MySQL.cpp
+	../mysql/MySQL.hpp
 	common.hpp
 	entry.cpp
 	Main.cpp
@@ -64,7 +67,7 @@ set(
 
 add_executable(tgvisd ${TGVISD_SOURCE})
 set_property(TARGET tgvisd PROPERTY CXX_STANDARD 20)
-target_link_libraries(tgvisd PRIVATE tgvisdtd pthread)
+target_link_libraries(tgvisd PRIVATE asan tgvisdtd mysqlclient pthread)
 target_compile_options(tgvisd PRIVATE
 	-ggdb3
 	-Wall
@@ -73,5 +76,6 @@ target_compile_options(tgvisd PRIVATE
 	-Wno-unused-parameter
 	-fno-omit-frame-pointer
 	-fstack-protector-strong
+	-fsanitize=address
 )
 ##################################################################

greentea/tgvisd/Main.cpp
@@ -32,7 +32,7 @@ __cold Main::Main(uint32_t api_id, const char *api_hash, const char *data_path):
 
 		/*
 		 * The assignment to scraperThread_ outside this
-		 * lambda might've not happened yet we the thread's
+		 * lambda might've not happened yet when the thread's
 		 * PC enters this lambda. Thus `this->scraperThread_`
 		 * may be NULL. Let's make sure the assignment has
 		 * been done before we run the scraper work.

greentea/tgvisd/Scraper.hpp
@@ -20,24 +20,28 @@ namespace tgvisd {
 
 class Scraper {
 private:
-	tgvisd::Td::Td *td_ = nullptr;
 	Main *main_ = nullptr;
 	std::thread *threadPtr_ = nullptr;
-	volatile bool stopScraper = false;
+	volatile bool stopScraper_ = false;
 
 	void scraperEventLoop(void);
-
-
-	td_api::object_ptr<td_api::chats> getChats(
-		td_api::object_ptr<td_api::ChatList> &&chatList, int32_t limit);
-
-	void scrapeChat(int64_t chat_id);
-	td_api::object_ptr<td_api::chat> getChat(int64_t chat_id);
 
 public:
 	Scraper(Main *main, std::thread *threadPtr);
 	~Scraper(void);
 	void run(void);
+
+
+	inline bool getStop(void)
+	{
+		return stopScraper_;
+	}
+
+
+	inline void doStop(void)
+	{
+		stopScraper_ = true;
+	}
 
 
 	inline Main *getMain(void)

greentea/tgvisd/Scraper.old.cpp
@@ -1,0 +1,401 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/*
+ * @author Ammar Faizi <ammarfaizi2@gmail.com> https://www.facebook.com/ammarfaizi2
+ * @license GPL-2.0-only
+ * @package tgvisd
+ *
+ * Copyright (C) 2021  Ammar Faizi <ammarfaizi2@gmail.com>
+ */
+
+#if defined(__linux__)
+	#include <unistd.h>
+	#include <pthread.h>
+#endif
+
+#include <mutex>
+#include <queue>
+#include <chrono>
+#include <cstdio>
+#include <cstring>
+#include <cassert>
+#include <cstdlib>
+#include <unordered_map>
+#include <condition_variable>
+#include <tgvisd/Scraper.hpp>
+
+#include <mysql/MySQL.hpp>
+
+namespace tgvisd {
+
+
+using std::mutex;
+using std::make_pair;
+using std::unique_lock;
+using std::unordered_map;
+using std::condition_variable;
+using namespace std::chrono_literals;
+
+
+Scraper::Scraper(Main *main, std::thread *threadPtr):
+	main_(main)
+{
+	assert(threadPtr);
+
+#if defined(__linux__)
+	pthread_t pt = threadPtr->native_handle();
+	pthread_setname_np(pt, "scraper-master");
+#endif
+}
+
+
+Scraper::~Scraper(void)
+{
+	pr_notice("Scraper task work is exiting...");
+}
+
+
+class WorkerPool;
+
+
+static void scraper_wrk(uint32_t wrk_id, WorkerPool *wrk);
+
+
+struct task_work {
+	int64_t chat_id;
+};
+
+
+class ScraperMaster
+{
+private:
+	Main 			*main_    = nullptr;
+	Scraper			*scr_     = nullptr;
+	WorkerPool		*wrkPool_ = nullptr;
+
+public:
+	inline ScraperMaster(Scraper *scr):
+		main_(scr->getMain()),
+		scr_(scr)
+	{
+	}
+
+
+	inline WorkerPool *getWrkPool(void)
+	{
+		return wrkPool_;
+	}
+
+
+	inline void setWrkPool(WorkerPool *wrkPool)
+	{
+		wrkPool_ = wrkPool;
+	}
+
+
+	inline void doStop(void)
+	{
+		scr_->doStop();
+	}
+
+
+	~ScraperMaster(void);
+
+
+	inline bool wrkShouldStop(void)
+	{
+		return scr_->getStop() || main_->getStop();
+	}
+
+
+	inline Main *getMain(void)
+	{
+		return main_;
+	}
+};
+
+
+
+class WorkerPool
+{
+private:
+	mysql::MySQL		**dbPool_   = nullptr;
+	std::thread		**wrkPool_  = nullptr;
+	struct task_work	*wrkTask_   = nullptr;
+	volatile bool		*wrkIsBusy_ = nullptr;
+	ScraperMaster		*master_    = nullptr;
+
+public:
+	mutex			wrkLock_;
+	condition_variable	wrkCond_;
+	uint32_t		wrkNum_ = 0;
+
+
+	inline ScraperMaster *getMaster(void)
+	{
+		return master_;
+	}
+
+
+	inline struct task_work *getTask(uint32_t wrk_id)
+	{
+		return &wrkTask_[wrk_id];
+	}
+
+	inline std::thread *getWrkPoolThread(uint32_t wrk_id)
+	{
+		return wrkPool_[wrk_id];
+	}
+
+
+private:
+	inline void initPool(void)
+	{
+		uint32_t i;
+
+		wrkPool_ = (std::thread **)calloc(wrkNum_, sizeof(*wrkPool_));
+		if (unlikely(!wrkPool_))
+			goto out_nomem;
+
+		wrkTask_ = (struct task_work *)calloc(wrkNum_, sizeof(*wrkTask_));
+		if (unlikely(!wrkTask_)) 
+			goto out_nomem;
+
+		wrkIsBusy_ = (volatile bool *)calloc(wrkNum_, sizeof(*wrkIsBusy_));
+		if (unlikely(!wrkIsBusy_))
+			goto out_nomem;
+
+		dbPool_ = (mysql::MySQL **)calloc(wrkNum_, sizeof(*dbPool_));
+		if (unlikely(!dbPool_))
+			goto out_nomem;
+
+		for (i = 0; i < wrkNum_; i++)
+			wrkPool_[i] = new std::thread(scraper_wrk, i, this);
+
+		return;
+
+	out_nomem:
+		cleanUp();
+		throw std::bad_alloc();
+	}
+
+
+public:
+	void cleanUp(void)
+	{
+		uint32_t i;
+
+		master_->doStop();
+
+		if (wrkPool_) {
+			for (i = 0; i < wrkNum_; i++) {
+				if (wrkPool_[i]) {
+					wrkPool_[i]->join();
+					delete wrkPool_[i];
+					wrkPool_[i] = nullptr;
+				}
+			}
+			free(wrkPool_);
+			wrkPool_ = nullptr;
+		}
+
+		if (dbPool_) {
+			for (i = 0; i < wrkNum_; i++) {
+				if (dbPool_[i]) {
+					delete dbPool_[i];
+					dbPool_ = nullptr;
+				}
+			}
+			free(dbPool_);
+			dbPool_ = nullptr;
+		}
+
+		if (wrkTask_) {
+			free(wrkTask_);
+			wrkTask_ = nullptr;
+		}
+
+		if (wrkIsBusy_) {
+			free((bool *)wrkIsBusy_);
+			wrkIsBusy_ = nullptr;
+		}
+	}
+
+
+	inline WorkerPool(uint32_t wrkNum, ScraperMaster *master):
+		master_(master),
+		wrkNum_(wrkNum)
+	{
+		initPool();
+	}
+
+
+	inline ~WorkerPool(void)
+	{
+		cleanUp();
+	}
+
+
+	inline int submitWork(struct task_work *task, uint32_t timeout = 30)
+	{
+		uint32_t i, secs = 0;
+		unique_lock lk(wrkLock_);
+
+		do {
+			if (unlikely(master_->wrkShouldStop()))
+				return -EOWNERDEAD;
+
+			for (i = 0; i < wrkNum_; i++) {
+				if (wrkIsBusy_[i])
+					continue;
+
+				/*
+				 * We find a free worker here. Give the
+				 * work to it!
+				 */
+				wrkTask_[i] = *task;
+				wrkIsBusy_[i] = true;
+				__sync_synchronize();
+				lk.unlock();
+				wrkCond_.notify_all();
+				return 0;
+			}
+			wrkCond_.wait_for(lk, 1000ms);
+		} while (++secs < timeout);
+
+		return -EAGAIN;
+	}
+
+
+	inline bool isWorkerBusy(uint32_t wrk_id)
+	{
+		return wrkIsBusy_[wrk_id];
+	}
+
+
+	inline void clearWork(uint32_t wrk_id)
+	{
+		unique_lock lk(wrkLock_);
+
+		memset(&wrkTask_[wrk_id], 0, sizeof(*wrkTask_));
+		wrkIsBusy_[wrk_id] = false;
+		__sync_synchronize();
+		wrkCond_.notify_all();
+	}
+};
+
+
+ScraperMaster::~ScraperMaster(void)
+{
+	scr_->doStop();
+	if (wrkPool_)
+		wrkPool_->cleanUp();
+}
+
+
+static void _scraper_wrk(uint32_t wrk_id, WorkerPool *wrk, ScraperMaster *master,
+			 unique_lock<mutex> &lk)
+{
+	struct task_work *tw;
+
+	lk.lock();
+	wrk->wrkCond_.wait_for(lk, 1000ms, [wrk_id, wrk](){
+		return wrk->isWorkerBusy(wrk_id);
+	});
+	lk.unlock();
+
+	tw = wrk->getTask(wrk_id);
+
+	printf("Scraping chat_id = %ld\n", tw->chat_id);
+	/* Do the work here... */
+	// sleep(1);
+
+	wrk->clearWork(wrk_id);
+}
+
+
+static int wait_for_pool_assignment(uint32_t wrk_id, WorkerPool *wrk,
+				    ScraperMaster *master)
+{
+	while (unlikely(!wrk->getWrkPoolThread(wrk_id))) {
+		cpu_relax();
+		if (unlikely(master->wrkShouldStop()))
+			return -ECONNABORTED;
+	}
+
+	return 0;
+}
+
+
+static void init_thread_name(uint32_t wrk_id, WorkerPool *wrk)
+{
+	char buf[sizeof("scraper-wrk-xxxxxxxxxx")];
+	std::thread *threadPtr = wrk->getWrkPoolThread(wrk_id);
+
+#if defined(__linux__)
+	pthread_t pt = threadPtr->native_handle();
+	snprintf(buf, sizeof(buf), "scraper-wrk-%u", wrk_id);
+	pthread_setname_np(pt, buf);
+#endif
+}
+
+
+static void scraper_wrk(uint32_t wrk_id, WorkerPool *wrk)
+{
+	unique_lock<mutex> lk(wrk->wrkLock_, std::defer_lock);
+	ScraperMaster *master = wrk->getMaster();
+
+	if (wait_for_pool_assignment(wrk_id, wrk, master))
+		return;
+
+	init_thread_name(wrk_id, wrk);
+
+	while (likely(!master->wrkShouldStop()))
+		_scraper_wrk(wrk_id, wrk, master, lk);
+}
+
+
+static void run_scraper(ScraperMaster *master)
+{
+	int64_t i;
+	WorkerPool *wrkPool;
+	struct task_work wrk;
+
+	wrkPool = master->getWrkPool();
+
+	for (i = 0; i < 1000; i++) {
+		wrk.chat_id = i;
+		wrkPool->submitWork(&wrk);
+	}
+}
+
+
+void Scraper::run(void)
+{
+	WorkerPool *wrkPool = nullptr;
+	ScraperMaster *master = nullptr;
+
+	try {
+		master  = new ScraperMaster(this);
+		wrkPool = new WorkerPool(4, master);
+
+		master->setWrkPool(wrkPool);
+		while (likely(!master->wrkShouldStop()))
+			run_scraper(master);
+
+	} catch (const std::runtime_error &e) {
+		pr_err("In scraper: std::runtime_error: %s", e.what());
+		main_->doStop();
+	} catch (const std::bad_alloc &e) {
+		pr_err("In scraper: Aiee... ENOMEM!");
+		main_->doStop();
+	}
+
+	if (master)
+		delete master;
+
+	if (wrkPool)
+		delete wrkPool;
+}
+
+
+} /* namespace tgvisd */

commit cf6baf10c6c646cf5f6a2937318fa1a55a1b5d25

greentea/tgvisd: Fix use-after-free bug
There is a use-after-free bug caused by race condition. This part is
the wrong thing to do:
```
  tasks_[kwrk_id].is_used = true;
  tasks_[kwrk_id].tw = std::move(*tw);
```
Note that on `_run_kworker()` we have this check:
```
  twl = kwrk_pool->getTW(kwrk_id);
  if (!twl->is_used)
          return;
```
If @twl->is_used is true but the @twl->tw still contains a dangling
pointer, then it can lead to use-after-free in the `_run_kworker()`.
Don't do that, make sure when @twl->is_used is set to true, the
@twl->tw has been changed with a valid pointer that `std::move(*tw);`
gives.

Fix for this is simple, make sure when `is_used` is set to true, the
`tw` does not contain a dangling pointer, so we can reorder the
assignment like this:
```
  tasks_[kwrk_id].tw = std::move(*tw);
  tasks_[kwrk_id].is_used = true;
```

Extra small changes:
  - Mark the `is_used` as volatile.
  - Reset the @tw.chat on `putTaskWork()` to reduce the work on
   `submitTaskWork()`.

Fixes: 71174b375e52b6513f6800c7ff6cb4dfc6d1b08e ("greetea/tgvisd: Implement scraper kworker")
Signed-off-by: Ammar Faizi <ammarfaizi2@gmail.com>

greentea/tgvisd/Scraper.cpp
@@ -69,7 +69,7 @@ struct task_work {
 
 
 struct task_work_list {
-	bool			is_used;
+	volatile bool		is_used = false;
 	struct task_work	tw;
 };
 
@@ -187,13 +187,13 @@ public:
 			ftLock_.unlock();
 			return -EAGAIN;
 		}
-
 		kwrk_id = freeThreads_.top();
 		freeThreads_.pop();
 		ftLock_.unlock();
 
-		tasks_[kwrk_id].is_used = true;
 		tasks_[kwrk_id].tw = std::move(*tw);
+		tasks_[kwrk_id].is_used = true;
+
 		ftCond_.notify_all();
 		return 0;
 	}
@@ -201,8 +201,10 @@ public:
 
 	inline void putTaskWork(uint16_t kwrk_id)
 	{
-		ftLock_.lock();
 		tasks_[kwrk_id].is_used = false;
+		tasks_[kwrk_id].tw.chat.reset();
+
+		ftLock_.lock();
 		freeThreads_.push(kwrk_id);
 		ftLock_.unlock();
 