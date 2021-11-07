TeaInside/green-tea-bot

commit 5ab179d5a7922acf168dd1b634a5e14890deadd8

greentea/tgvisd: Implement several td methods on kworker
Let's move them from Scraper.cpp to KWorker.hpp.

Signed-off-by: Ammar Faizi <ammarfaizi2@gmail.com>

greentea/tgvisd/KWorker.cpp
@@ -34,6 +34,7 @@ using namespace std::chrono_literals;
 
 __cold KWorker::KWorker(Main *main, uint32_t maxThPool, uint32_t maxDbPool,
 			uint32_t maxNRTasks):
+	td_(main->getTd()),
 	main_(main),
 	maxThPool_(maxThPool),
 	activeThPool_(0)

greentea/tgvisd/KWorker.hpp
@@ -98,6 +98,7 @@ class KWorker
 {
 private:
 	volatile bool		stop_          = false;
+	tgvisd::Td::Td		*td_           = nullptr;
 	Main			*main_         = nullptr;
 	struct thpool		*thPool_       = nullptr;
 	struct dbpool		*dbPool_       = nullptr;
@@ -139,11 +140,13 @@ public:
 		cleanUp();
 	}
 
+
 	KWorker(Main *main, uint32_t maxThPool = 16, uint32_t maxDbPool = 256,
 		uint32_t maxNRTasks = 4096);
 	int submitTaskWork(struct task_work *tw);
 	mysql::MySQL *getDbPool(void);
 	void putDbPool(mysql::MySQL *db);
+
 
 	template<class Rep, class Period>
 	inline void waitQueue(const duration<Rep, Period> &rel_time)
@@ -152,15 +155,18 @@ public:
 		taskPutCond_.wait_for(lk, rel_time);
 	}
 
+
 	inline bool shouldStop(void)
 	{
 		return unlikely(stop_ || main_->getStop());
 	}
 
+
 	inline void run(void)
 	{
 		runMasterKWorker();
 	}
+
 
 	inline void stop(void)
 	{
@@ -171,12 +177,69 @@ public:
 	}
 
 
+	inline Main *getMain(void)
+	{
+		return main_;
+	}
+
+
+	inline tgvisd::Td::Td *getTd(void)
+	{
+		return td_;
+	}
+
+
 	inline static void setMasterThreadName(std::thread *task)
 	{
 #if defined(__linux__)
 		pthread_t pt = task->native_handle();
 		pthread_setname_np(pt, "tgvkwrk-master");
 #endif
+	}
+
+
+	static constexpr uint32_t query_sync_timeout = 150;
+
+
+	inline td_api::object_ptr<td_api::chats> getChats(
+		td_api::object_ptr<td_api::ChatList> &&chatList, int32_t limit)
+	{
+		return td_->send_query_sync<td_api::getChats, td_api::chats>(
+			td_api::make_object<td_api::getChats>(
+				std::move(chatList),
+				limit
+			),
+			query_sync_timeout
+		);
+	}
+
+
+	inline td_api::object_ptr<td_api::chat> getChat(int64_t chat_id)
+	{
+		return td_->send_query_sync<td_api::getChat, td_api::chat>(
+			td_api::make_object<td_api::getChat>(chat_id),
+			query_sync_timeout
+		);
+	}
+
+
+	inline td_api::object_ptr<td_api::messages> getChatHistory(
+				int64_t chat_id,
+				int64_t from_msg_id,
+				int32_t offset,
+				int32_t limit,
+				bool only_local = false)
+	{
+		return td_->send_query_sync<td_api::getChatHistory, td_api::messages>(
+			td_api::make_object<td_api::getChatHistory>(
+				chat_id,
+				from_msg_id,
+				offset,
+				limit,
+				only_local
+			),
+			query_sync_timeout
+		);
 	}
 };
 

commit 8f4cc699ea52f1c6e953623a05d40df3b86f6e48

greentea/tgvisd: Add join queue to handle tgvkwrk-%u exit
When the `KWorker::runThreadPool()` is exiting, it can't call
`thread->join()` itself because the task to be waited is itself.

The workaround for this was using `thread->detach()` and free
the associated resource with it. But it can't guarantee the
task has exited right after the resource gets freed, so it leads
to race condition with the `KWorker::getThPool()`. This race
condition caused UAF bug as well. ASAN caught that and had me
debugged this for couple of hours pondering the call traces!

In that case, we can't hold `thPoolLock_` neither. Because we
are going to exit, nobody can release the lock when it exits.
The master kworker may do, but nobody notifies it.

^ We can't do that again!

Better solution for this is:
 - Add a queue work for master kworker to clean these up.
 - Call `thread->join()` from the master kworker based on
   the queue work.
 - After `thread->join()`, we can guarantee that the task
   has exited, so it resolves all the problems went here.

It consumes extra space for `std::queue<uint32_t>` and
`std::mutex`. But I don't see that as a problem.

This is the best way I can think of right now. If you think
there is a better workaround for this, you can email me and
argue with your technical standpoint.

Fixes: 5ab179d5a7922acf168dd1b634a5e14890deadd8 ("greentea/tgvisd: Implement several td methods on kworker")
Signed-off-by: Ammar Faizi <ammarfaizi2@gmail.com>

greentea/tgvisd/KWorker.cpp
@@ -233,20 +233,43 @@ void KWorker::runThreadPool(struct thpool *pool)
 	}
 
 out:
-	thPoolLock_.lock();
-	if (pool->thread) {
-		pool->thread->detach();
-		delete pool->thread;
-		pool->thread = nullptr;
-	}
-	thPoolStk_.push(pool->idx);
-	thPoolLock_.unlock();
+	joinQueueLock_.lock();
+	joinQueue_.push(pool->idx);
+	joinQueueLock_.unlock();
+	masterCond_.notify_one();
+
 	activeThPool_--;
 	return;
 
 idle_exit:
 	pr_notice("tgvkwrk-%u is exiting due to inactivity...", pool->idx);
 	goto out;
+}
+
+
+void KWorker::handleJoinQueue(void)
+{
+	joinQueueLock_.lock();
+	while (!joinQueue_.empty()) {
+		uint32_t idx;
+
+		idx = joinQueue_.front();
+		joinQueue_.pop();
+		joinQueueLock_.unlock();
+
+		pr_notice("tgvkwrk-master: Joining tgvkwrk-%u...", idx);
+		thPool_[idx].stop = true;
+		thPool_[idx].thread->join();
+		delete thPool_[idx].thread;
+		thPool_[idx].thread = nullptr;
+
+		thPoolLock_.lock();
+		thPoolStk_.push(idx);
+		thPoolLock_.unlock();
+
+		joinQueueLock_.lock();
+	}
+	joinQueueLock_.unlock();
 }
 
 
@@ -262,6 +285,8 @@ void KWorker::runMasterKWorker(void)
 		lk.lock();
 		masterCond_.wait_for(lk, 10000ms);
 		lk.unlock();
+
+		handleJoinQueue();
 
 		act_thread    = activeThPool_.load();
 		num_of_queues = tasksQueue_.size();

greentea/tgvisd/KWorker.hpp
@@ -119,6 +119,9 @@ private:
 	std::stack<uint32_t>	freeTask_;
 	std::queue<uint32_t>	tasksQueue_;
 
+	std::mutex		joinQueueLock_;
+	std::queue<uint32_t>	joinQueue_;
+
 	const char		*sqlHost_   = nullptr;
 	const char		*sqlUser_   = nullptr;
 	const char		*sqlPass_   = nullptr;
@@ -128,6 +131,7 @@ private:
 
 	void cleanUp(void);
 	void runMasterKWorker(void);
+	void handleJoinQueue(void);
 	void runThreadPool(struct thpool *pool);
 	struct task_work *getTaskWork(void);
 	struct thpool *getThPool(void);