TeaInside/green-tea-bot

commit b1e6736585efbaaf975bb79ac34c699eafdb8b86

greentea/tgvisd: Implement chat lock by chat_id
Note that when we're working with database, we may have multiple
threads doing work to the same chat_id, thus it can cause race
condition problem. Currently we don't have a good workaround for
the MySQL side, so let's create a map lock mutex on the app, this
should have better latency as well, as locking on MySQL will
require network communication, which is very likely slower than
SMP synchronization.

Signed-off-by: Ammar Faizi <ammarfaizi2@gmail.com>

greentea/tgvisd/Scraper.cpp
@@ -232,6 +232,9 @@ private:
 	Scraper			*scraper_   = nullptr;
 	KWorkerPool		*kwrkPool_  = nullptr;
 
+	mutex				clmLock;
+	unordered_map<int64_t, mutex *>	chatLockMap_;
+	volatile bool			dropChatLock_ = false;
 public:
 	inline tgvisd::Td::Td *getTd(void)
 	{
@@ -262,6 +265,44 @@ public:
 	{
 		main_ = scraper->getMain();
 		td_   = main_->getTd();
+	}
+
+
+	inline ~KWorker(void)
+	{
+		dropChatLock_ = true;
+		for (auto &i: chatLockMap_) {
+			mutex *mut = i.second;
+			if (!mut)
+				continue;
+
+			mut->lock();
+			mut->unlock();
+			delete mut;
+			i.second = nullptr;
+		}
+	}
+
+
+	inline mutex *getChatLock(int64_t chat_id)
+		__acquires(&clmLock)
+		__releases(&clmLock)
+	{
+		mutex *ret;
+
+		if (unlikely(dropChatLock_))
+			return nullptr;
+
+		clmLock.lock();
+		const auto &i = chatLockMap_.find(chat_id);
+		if (i == chatLockMap_.end()) {
+			ret = new mutex;
+			chatLockMap_.emplace(chat_id, ret);
+		} else {
+			ret = i->second;
+		}
+		clmLock.unlock();
+		return ret;
 	}
 
 
@@ -349,8 +390,28 @@ static void wait_for_pool_assignment(uint32_t kwrk_id, KWorkerPool *kwrk_pool)
 }
 
 
+static void __run_kworker(uint32_t kwrk_id, KWorkerPool *kwrk_pool,
+			  KWorker *kwrk, struct task_work *tw)
+{
+	int64_t chat_id;
+	mutex *chatLock;
+
+	chat_id  = tw->chat->id_;
+	chatLock = kwrk->getChatLock(chat_id);
+	if (unlikely(!chatLock)) {
+		pr_err("[tid=%d] Cannot get lock for chat_id = %ld", gettid(),
+		       chat_id);
+		return;
+	}
+
+	chatLock->lock();
+	pr_notice("[tid=%d] Scraping chat_id = %ld", gettid(), chat_id);
+	chatLock->unlock();
+}
+
+
 static void _run_kworker(uint32_t kwrk_id, KWorkerPool *kwrk_pool,
-			 std::unique_lock<mutex> &lk)
+			 KWorker *kwrk, std::unique_lock<mutex> &lk)
 {
 	struct task_work_list *twl;
 
@@ -362,8 +423,7 @@ static void _run_kworker(uint32_t kwrk_id, KWorkerPool *kwrk_pool,
 	if (!twl->is_used)
 		return;
 
-	pr_notice("[tid=%d] Scraping chat_id = %ld", gettid(), twl->tw.chat->id_);
-	sleep(1);
+	__run_kworker(kwrk_id, kwrk_pool, kwrk, &twl->tw);
 	kwrk_pool->putTaskWork(kwrk_id);
 }
 
@@ -376,7 +436,7 @@ static void run_kworker(uint32_t kwrk_id, KWorkerPool *kwrk_pool)
 	wait_for_pool_assignment(kwrk_id, kwrk_pool);
 
 	while (!kwrk->kworkerShouldStop())
-		_run_kworker(kwrk_id, kwrk_pool, lk);
+		_run_kworker(kwrk_id, kwrk_pool, kwrk, lk);
 }
 
 
@@ -400,10 +460,11 @@ static int __run_kworker_event_loop(KWorker *kwrk, KWorkerPool *kwrk_pool,
 		}
 
 		ret = kwrk_pool->submitTaskWork(&tw);
-		if (ret == -EAGAIN)
+		if (ret == -EAGAIN) {
 			kwrk_pool->waitForWorker();
-		else
+		} else {
 			break;
+		}
 	}
 
 	return 0;
@@ -416,14 +477,14 @@ static void _run_kworker_event_loop(KWorker *kwrk, KWorkerPool *kwrk_pool)
 	int64_t chat_id;
 	auto chats = kwrk->getChats(nullptr, 300);
 
-	if (!chats)
+	if (unlikely(!chats))
 		return;
 
 	for (i = 0; i < chats->total_count_; i++) {
 		chat_id = chats->chat_ids_[i];
 		auto chat = kwrk->getChat(chat_id);
 
-		if (!chat)
+		if (unlikely(!chat))
 			continue;
 		if (chat->type_->get_id() != td_api::chatTypeSupergroup::ID)
 			continue;

commit f96a2d8a7a018321585709eb75b96339ea8c5a8f

greentea/tgvisd: [v2] Implement chat lock by chat_id
Note that when we're working with database, we may have multiple
threads doing work to the same chat_id, thus it can cause race
condition problem. Currently we don't have a good workaround for
the MySQL side, so let's create a map lock mutex on the app, this
should have better latency as well, as locking on MySQL will
require network communication, which is very likely slower than
mutex synchronization in memory.

Fixes: b1e6736585efbaaf975bb79ac34c699eafdb8b86 ("greentea/tgvisd: Implement chat lock by chat_id")
Signed-off-by: Ammar Faizi <ammarfaizi2@gmail.com>

greentea/tgvisd/KWorker.cpp
@@ -225,8 +225,9 @@ void KWorker::runThreadPool(struct thpool *pool)
 			continue;
 		}
 
-		data.tw   = tw;
+		data.tw = tw;
 		data.kwrk = this;
+		data.current = pool;
 		pool->setUninterruptible();
 		tw->func(&data);
 		putTaskWork(tw);
@@ -273,6 +274,26 @@ void KWorker::handleJoinQueue(void)
 		joinQueueLock_.lock();
 	}
 	joinQueueLock_.unlock();
+}
+
+
+std::mutex *KWorker::getChatLock(int64_t tg_chat_id)
+{
+	std::mutex *ret;
+
+	if (unlikely(dropChatLock_ || shouldStop()))
+		return nullptr;
+
+	clmLock_.lock();
+	const auto &it = chatLockMap_.find(tg_chat_id);
+	if (it == chatLockMap_.end()) {
+		ret = new std::mutex;
+		chatLockMap_.emplace(tg_chat_id, ret);
+	} else {
+		ret = it->second;
+	}
+	clmLock_.unlock();
+	return ret;
 }
 
 
@@ -344,6 +365,8 @@ __cold void KWorker::cleanUp(void)
 {
 	uint32_t i;
 
+	dropChatLock_ = true;
+
 	if (thPool_) {
 		joinQueueLock_.lock();
 		thPoolLock_.lock();
@@ -386,6 +409,45 @@ __cold void KWorker::cleanUp(void)
 		delete[] tasks_;
 		tasks_ = nullptr;
 	}
+
+	clmLock_.lock();
+	for (auto &i: chatLockMap_) {
+		std::mutex *mut;
+		mut = i.second;
+		mut->lock();
+		mut->unlock();
+		delete mut;
+		i.second = nullptr;
+	}
+	clmLock_.unlock();
+}
+
+
+void thpool::setInterruptible(void)
+{
+#if defined(__linux__)
+	pthread_t pt;
+	char buf[sizeof("tgvkwrk-xxxxxxxxx")];
+	if (unlikely(!this->thread))
+		return;
+	pt = this->thread->native_handle();
+	snprintf(buf, sizeof(buf), "tgvkwrk-%u", idx);
+	pthread_setname_np(pt, buf);
+#endif
+}
+
+
+void thpool::setUninterruptible(void)
+{
+#if defined(__linux__)
+	pthread_t pt;
+	char buf[sizeof("tgvkwrk-D-xxxxxxxxx")];
+	if (unlikely(!this->thread))
+		return;
+	pt = this->thread->native_handle();
+	snprintf(buf, sizeof(buf), "tgvkwrk-D-%u", idx);
+	pthread_setname_np(pt, buf);
+#endif
 }
 
 

greentea/tgvisd/KWorker.hpp
@@ -47,32 +47,8 @@ struct thpool {
 	std::thread				*thread = nullptr;
 	uint32_t				idx;
 
-	inline void setInterruptible(void)
-	{
-#if defined(__linux__)
-		pthread_t pt;
-		char buf[sizeof("tgvkwrk-xxxxxxxxx")];
-		if (unlikely(!this->thread))
-			return;
-		pt = this->thread->native_handle();
-		snprintf(buf, sizeof(buf), "tgvkwrk-%u", idx);
-		pthread_setname_np(pt, buf);
-#endif
-	}
-
-
-	inline void setUninterruptible(void)
-	{
-#if defined(__linux__)
-		pthread_t pt;
-		char buf[sizeof("tgvkwrk-D-xxxxxxxxx")];
-		if (unlikely(!this->thread))
-			return;
-		pt = this->thread->native_handle();
-		snprintf(buf, sizeof(buf), "tgvkwrk-D-%u", idx);
-		pthread_setname_np(pt, buf);
-#endif
-	}
+	void setInterruptible(void);
+	void setUninterruptible(void);
 };
 
 
@@ -83,8 +59,9 @@ struct dbpool {
 
 
 struct tw_data {
-	KWorker					*kwrk;
 	struct task_work			*tw;
+	KWorker					*kwrk;
+	struct thpool				*current;
 };
 
 
@@ -99,6 +76,7 @@ class KWorker
 {
 private:
 	volatile bool		stop_          = false;
+	volatile bool		dropChatLock_  = false;
 	tgvisd::Td::Td		*td_           = nullptr;
 	Main			*main_         = nullptr;
 	struct thpool		*thPool_       = nullptr;
@@ -123,12 +101,14 @@ private:
 	std::mutex		joinQueueLock_;
 	std::queue<uint32_t>	joinQueue_;
 
+	std::mutex					clmLock_;
+	std::unordered_map<int64_t, std::mutex *>	chatLockMap_;
+
 	const char		*sqlHost_   = nullptr;
 	const char		*sqlUser_   = nullptr;
 	const char		*sqlPass_   = nullptr;
 	const char		*sqlDBName_ = nullptr;
 	uint16_t		sqlPort_    = 0u;
-
 
 	void cleanUp(void);
 	void runMasterKWorker(void);
@@ -151,6 +131,7 @@ public:
 	int submitTaskWork(struct task_work *tw);
 	mysql::MySQL *getDbPool(void);
 	void putDbPool(mysql::MySQL *db);
+	std::mutex *getChatLock(int64_t tg_chat_id);
 
 
 	template<class Rep, class Period>

greentea/tgvisd/Scraper.cpp
@@ -85,9 +85,10 @@ __hot void Scraper::visit_chat(td_api::object_ptr<td_api::chat> &chat)
 	int ret;
 	struct task_work tw;
 
-	tw.func = [this, &chat](struct tw_data *data){
-		_visit_chat(data, std::move(chat));
+	tw.func = [this](struct tw_data *data){
+		this->_visit_chat(data, data->tw->data);
 	};
+	tw.data = std::move(chat);
 
 	do {
 		if (shouldStop())
@@ -104,8 +105,59 @@ __hot void Scraper::visit_chat(td_api::object_ptr<td_api::chat> &chat)
 __hot void Scraper::_visit_chat(struct tw_data *data,
 				td_api::object_ptr<td_api::chat> &chat)
 {
-	printf("Scraping %ld...\n", chat->id_);
-	sleep(2);
+	int32_t count, i;
+	struct thpool *current = data->current;
+	std::mutex *chat_lock;
+
+	pr_notice("Scraping messages from (%ld) [%s]...", chat->id_,
+		  chat->title_.c_str());
+
+	chat_lock = kworker_->getChatLock(chat->id_);
+	if (unlikely(!chat_lock)) {
+		pr_notice("Could not get chat lock (%ld) [%s]", chat->id_,
+			  chat->title_.c_str());
+		return;
+	}
+
+	auto messages = kworker_->getChatHistory(chat->id_, 0, 0, 300);
+	if (unlikely(!messages)) {
+		pr_notice("Could not get message history from (%ld) [%s]",
+			  chat->id_, chat->title_.c_str());
+		return;
+	}
+
+	count = messages->total_count_;
+	current->setInterruptible();
+	for (i = 0; i < count; i++) {
+		if (shouldStop())
+			break;
+
+		auto &msg = messages->messages_[i];
+		if (unlikely(!msg))
+			continue;
+
+		current->setUninterruptible();
+		chat_lock->lock();
+		extract_msg_content(msg);
+		chat_lock->unlock();
+		current->setInterruptible();
+	}
+}
+
+
+void Scraper::extract_msg_content(td_api::object_ptr<td_api::message> &msg)
+{
+	auto &content = msg->content_;
+
+	if (unlikely(!content))
+		return;
+
+	if (content->get_id() != td_api::messageText::ID)
+		return;
+
+	auto &text = static_cast<td_api::messageText &>(*content);
+	pr_notice("text = %s", text.text_->text_.c_str());
+	sleep(3);
 }
 
 

greentea/tgvisd/Scraper.hpp
@@ -29,6 +29,7 @@ private:
 	void visit_chat(td_api::object_ptr<td_api::chat> &chat);
 	void _visit_chat(struct tw_data *data,
 			 td_api::object_ptr<td_api::chat> &chat);
+	void extract_msg_content(td_api::object_ptr<td_api::message> &msg);
 
 public:
 	Scraper(Main *main);