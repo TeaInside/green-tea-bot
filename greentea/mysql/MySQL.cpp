TeaInside/green-tea-bot

commit e9565018fe3c125e0e2bed9ff598ee9ac5d3c20e

greentea/mysql: Add default constructor
Add default constructor to make it easily creating array of this
wrapper instances.

Signed-off-by: Ammar Faizi <ammarfaizi2@gmail.com>

greentea/mysql/MySQL.hpp
@@ -244,9 +244,15 @@ private:
 	uint16_t port_ = 0;
 
 public:
-
+	MySQL(void) = default;
 	MySQL(const char *host, const char *user, const char *passwd,
 	      const char *dbname);
+
+	inline void init(const char *host, const char *user, const char *passwd,
+			 const char *dbname)
+	{
+		MySQL(host, user, passwd, dbname);
+	}
 
 	bool connect(void) noexcept;
 	MySQLRes *storeResult(void) noexcept;

commit 3e185f6ead00dcc278ede11d3344ceec53120dec

greentea/mysql: Call `init()` function from constructor
This fixes null pointer dereference bug.

Fixes: e9565018fe3c125e0e2bed9ff598ee9ac5d3c20e ("greentea/mysql: Add default constructor")
Signed-off-by: Ammar Faizi <ammarfaizi2@gmail.com>

greentea/mysql/MySQL.cpp
@@ -12,12 +12,19 @@ namespace mysql {
 
 
 MySQL::MySQL(const char *host, const char *user, const char *passwd,
-	     const char *dbname):
-	host_(host),
-	user_(user),
-	passwd_(passwd),
-	dbname_(dbname)
+	     const char *dbname)
 {
+	init(host, user, passwd, dbname);
+}
+
+
+void MySQL::init(const char *host, const char *user, const char *passwd,
+		 const char *dbname)
+{
+	host_ = host;
+	user_ = user;
+	passwd_ = passwd;
+	dbname_ = dbname;
 	conn_ = mysql_init(NULL);
 	if (unlikely(!conn_))
 		throw std::runtime_error("Cannot init mysql on mysql_init()");

greentea/mysql/MySQL.hpp
@@ -248,11 +248,8 @@ public:
 	MySQL(const char *host, const char *user, const char *passwd,
 	      const char *dbname);
 
-	inline void init(const char *host, const char *user, const char *passwd,
-			 const char *dbname)
-	{
-		MySQL(host, user, passwd, dbname);
-	}
+	void init(const char *host, const char *user, const char *passwd,
+		  const char *dbname);
 
 	bool connect(void) noexcept;
 	MySQLRes *storeResult(void) noexcept;