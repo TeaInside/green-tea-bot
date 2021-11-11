TeaInside/green-tea-bot

commit a06336919192a31b491f230f530f83a360bc6d40

greentea/tgvisd: Td: Fix thread cancelation bug
Alviro reports that he found a segmentation fault on the lambda that
the `query_sync_callback()` creates.

The story behind the bug comes from `Td::send_query_sync()`. It
creates a callback that tdlib is supposed to process. The problem with
that callback is that, it holds a pointer to a local variable on
`Td::send_query_sync()`'s stack.

When we get a cancelation from the main thread, `Td::send_query_sync()`
returns without even care whether the callback still holds a pointer to
its local variable. Once we return, the local variable is invalid, its
lifetime ends, results in undefined behavior from the callback.

Fix this by making a promise. Wrap the argument with a struct that
lives on the heap. Then add an extra variable `uint8_t cleaner`. This
variabe is protected with a mutex, if the value is `QSD_CLEAN_FROM_SQS`,
we do the clean up from `Td::send_query_sync()`. Otherwise we do the
clean up from the callback.

Link: https://t.me/TeaInside/45071
Reported-by: Alviro Iskandar Setiawan <alviro.iskandar@gmail.com>
Signed-off-by: Ammar Faizi <ammarfaizi2@gmail.com>
Tested-by: Alviro Iskandar Setiawan <alviro.iskandar@gmail.com>

greentea/tgvisd/Td/Td.hpp
@@ -113,8 +113,8 @@ private:
 public:
 	Td(uint32_t api_id, const char *api_hash, const char *data_path);
 
-	void send_query(td_api::object_ptr<td_api::Function> f,
-			function<void(Object)> handler);
+	uint64_t send_query(td_api::object_ptr<td_api::Function> f,
+			    function<void(Object)> handler);
 
 	void loop(int timeout);
 	void close(void);
@@ -142,17 +142,40 @@ public:
 };
 
 
+#define QSD_CLEAN_FROM_SQS	(0u)
+#define QSD_CLEAN_FROM_CALLBACK	(1u)
 template <typename U>
-static inline auto query_sync_callback(condition_variable *cond,
-				       volatile bool *finished,
-				       td_api::object_ptr<U> *ret,
-				       td_api::object_ptr<td_api::error> *err)
+struct query_sync_data {
+	condition_variable			cond;
+	std::mutex				mutex;
+	std::unique_lock<std::mutex>		lock;
+	td_api::object_ptr<U>			ret;
+	td_api::object_ptr<td_api::error>	*err;
+	volatile bool				finished = false;
+	volatile uint8_t			cleaner  = QSD_CLEAN_FROM_SQS;
+
+	query_sync_data(void):
+		lock(this->mutex, std::defer_lock)
+	{
+	}
+};
+
+
+template <typename U>
+static inline auto query_sync_callback(query_sync_data<U> *data)
 {
 	return [=](td_api::object_ptr<td_api::Object> obj) {
 
+		data->mutex.lock();
+		if (unlikely(data->cleaner == QSD_CLEAN_FROM_CALLBACK)) {
+			data->mutex.unlock();
+			delete data;
+			return;
+		}
+
 		if (obj->get_id() == td_api::error::ID) {
-			if (err) {
-				*err = td::move_tl_object_as<td_api::error>(obj);
+			if (data->err) {
+				*data->err = td::move_tl_object_as<td_api::error>(obj);
 				pr_err("Got error on query_sync_callback");
 			}
 			goto out;
@@ -163,11 +186,11 @@ static inline auto query_sync_callback(condition_variable *cond,
 			goto out;
 		}
 
-		*ret = td::move_tl_object_as<U>(obj);
-
 	out:
-		*finished = true;
-		cond->notify_one();
+		data->ret = td::move_tl_object_as<U>(obj);
+		data->finished = true;
+		data->cond.notify_one();
+		data->mutex.unlock();
 	};
 }
 
@@ -189,27 +212,26 @@ td_api::object_ptr<U> Td::send_query_sync(td_api::object_ptr<T> method,
 					  uint32_t timeout,
 					  td_api::object_ptr<td_api::error> *err)
 {
-	mutex mut;
-	condition_variable cond;
-	unique_lock<mutex> lock(mut, std::defer_lock);
 	td_api::object_ptr<U> ret;
-	volatile bool finished = false;
+	query_sync_data<U> *data = new query_sync_data<U>();
+	data->err = err;
 
 	uint32_t secs = 0;
 	const uint32_t warnOnSecs = 120;
 
-	send_query(std::move(method), query_sync_callback<U>(&cond, &finished,
-							     &ret, err));
+	send_query(std::move(method), query_sync_callback<U>(data));
 
-	lock.lock();
-	while (!finished) {
+	data->lock.lock();
+	while (!data->finished) {
 
-		if (unlikely(getCancelDelayedWork()))
+		if (unlikely(getCancelDelayedWork())) {
+			data->cleaner = QSD_CLEAN_FROM_CALLBACK;
 			break;
+		}
 
-		cond.wait_for(lock, 1000ms);
+		data->cond.wait_for(data->lock, 1000ms);
 
-		if (likely(finished))
+		if (likely(data->finished))
 			break;
 
 		if (unlikely(++secs >= warnOnSecs))
@@ -224,6 +246,11 @@ td_api::object_ptr<U> Td::send_query_sync(td_api::object_ptr<T> method,
 			break;
 		}
 	}
+	ret = std::move(data->ret);
+	data->lock.unlock();
+
+	if (likely(data->cleaner == QSD_CLEAN_FROM_SQS))
+		delete data;
 
 	return ret;
 }

greentea/tgvisd/Td/Td.cpp
@@ -36,8 +36,8 @@ __cold Td::Td(uint32_t api_id, const char *api_hash, const char *data_path):
 }
 
 
-__hot void Td::send_query(td_api::object_ptr<td_api::Function> f,
-			  function<void(Object)> handler)
+__hot uint64_t Td::send_query(td_api::object_ptr<td_api::Function> f,
+			      function<void(Object)> handler)
 {
 	uint64_t query_id;
 
@@ -49,6 +49,7 @@ __hot void Td::send_query(td_api::object_ptr<td_api::Function> f,
 	}
 
 	client_manager_->send(client_id_, query_id, std::move(f));
+	return query_id;
 }
 
 

commit b43a781bc8c74692d120456f1439a6e0a5d782c4

greentea/tgvisd: Td: Set QSD_CLEAN_FROM_CALLBACK on timeout
Avoid the same issue with commit a06336919192a3 ("greentea/tgvisd:
Td: Fix thread cancelation bug").

Fixes: a06336919192a3 ("greentea/tgvisd: Td: Fix thread cancelation bug")
Signed-off-by: Alviro Iskandar Setiawan <alviro.iskandar@gmail.com>
Link: https://t.me/TeaInside/45120
Signed-off-by: Ammar Faizi <ammarfaizi2@gmail.com>

greentea/tgvisd/Td/Td.hpp
@@ -238,6 +238,7 @@ td_api::object_ptr<U> Td::send_query_sync(td_api::object_ptr<T> method,
 			if (unlikely(secs >= timeout)) {
 				pr_notice("Warning: send_query_sync() reached "
 					  "timeout after %u seconds", secs);
+				data->cleaner = QSD_CLEAN_FROM_CALLBACK;
 				break;
 			}
 		} else {