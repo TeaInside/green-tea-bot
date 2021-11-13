TeaInside/green-tea-bot

commit b4a9d289fa66085ded9bd69c0e6154e447fb439a

greentea/tgvisd: entry: Add try and catch on main()
Make it easy to debug uncaught exception.

Signed-off-by: Alviro Iskandar Setiawan <alviro.iskandar@gmail.com>

greentea/tgvisd/entry.cpp
@@ -14,6 +14,7 @@
 
 int main(void)
 {
+	int ret;
 	const char *api_id, *api_hash, *data_path;
 
 	api_id = getenv("TGVISD_API_ID");
@@ -34,6 +35,12 @@ int main(void)
 		return 1;
 	}
 
-	tgvisd::Main mm((uint32_t)atoi(api_id), api_hash, data_path);
-	return mm.run();
+	try {
+		tgvisd::Main mm((uint32_t)atoi(api_id), api_hash, data_path);
+		ret = mm.run();
+	} catch (const std::runtime_exception &e) {
+		printf("Err: %s\n", e.what());
+		throw e;
+	}
+	return ret;
 }

commit 865bd5b784e0a97c2c5ec70dee7feed90d95822d

greentea/tgvisd: Fix wrong try and catch
Sorry, I was rushed and didn't test it properly. The catch block
should catch std::runtime_error not std::runtime_exception.

Fixes: b4a9d289fa66085d ("greentea/tgvisd: entry: Add try and catch on main()")
Signed-off-by: Alviro Iskandar Setiawan <alviro.iskandar@gmail.com>
Tested-by: Ammar Faizi <ammarfaizi2@gmail.com>
Link: https://t.me/TeaInside/45567
Signed-off-by: Ammar Faizi <ammarfaizi2@gmail.com>

greentea/tgvisd/entry.cpp
@@ -9,6 +9,7 @@
 
 #include <cstdio>
 #include <cstdlib>
+#include <stdexcept>
 #include <tgvisd/Main.hpp>
 
 
@@ -38,7 +39,7 @@ int main(void)
 	try {
 		tgvisd::Main mm((uint32_t)atoi(api_id), api_hash, data_path);
 		ret = mm.run();
-	} catch (const std::runtime_exception &e) {
+	} catch (const std::runtime_error &e) {
 		printf("Err: %s\n", e.what());
 		throw e;
 	}