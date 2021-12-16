ad348f7160a89360f5be1a2eda92fd495b42f8da
Ilham Muttaqien <ilham.muttaqien@students.amikom.ac.id>
2021-12-16 22:33:42 +0700

    web: Integrate Group List API
    Signed-off-by: Ilham Muttaqien <ilham.muttaqien@students.amikom.ac.id>

--- web/green-tea/components/Group.js
+++ web/green-tea/components/Group.js
@@ -1,9 +1,9 @@
-function Group({ namaGroup, lastMessage }) {
+function Group({ groupName, groupId, lastMessage }) {
     return (
         <div className="flex items-center cursor-pointer p-2 hover:bg-gray-200">
             <img className="w-12 h-12 rounded-full" src="profile.jpeg" alt="" />
             <div className="ml-4">
-                <h2>{namaGroup}</h2>
+                <h2>{"(" + groupId + ") " + "" + groupName}</h2>
                 <p className="text-gray-400">{lastMessage}</p>
             </div>
         </div>
