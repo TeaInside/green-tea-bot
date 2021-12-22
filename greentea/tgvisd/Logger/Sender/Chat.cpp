69620d9f7439c9ef288a17016506541796e3616f
Alviro Iskandar Setiawan <alviro.iskandar@gmail.com>
2021-12-23 00:47:05 +0700

    greentea/tgvisd: Logger: Add sender abstractions
    This adds sender abstractions for database normalization. The main
    issue this patch addresses is that the sender may not be a user, it
    can be a group or channel. So we normalize it here.
    
    Currently, there are 2 types of sender:
      1. User
      2. Chat (this includes group and channel)
    
    This patch only creates new files for the abstractions without
    changing the functionality. We will use it in the later patches.
    
    Signed-off-by: Alviro Iskandar Setiawan <alviro.iskandar@gmail.com>
    Link: https://t.me/TeaInside/47415
    Signed-off-by: Ammar Faizi <ammarfaizi2@gmail.com>

---
+++ greentea/tgvisd/Logger/Sender/Chat.cpp
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/*
+ * @author Alviro Iskandar Setiawan <alviro.iskandar@gmail.com>
+ * @license GPL-2.0-only
+ * @package tgvisd::Logger::Sender
+ *
+ * Copyright (C) 2021  Alviro Iskandar Setiawan <alviro.iskandar@gmail.com>
+ */
+
+#include <tgvisd/Logger/Sender/Chat.hpp>
+
+namespace tgvisd::Logger::Sender {
+
+
+} /* namespace tgvisd::Logger::Sender */
---
+++ greentea/tgvisd/Logger/SenderFoundation.hpp
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/*
+ * @author Alviro Iskandar Setiawan <alviro.iskandar@gmail.com>
+ * @license GPL-2.0-only
+ * @package tgvisd::Logger::Chat
+ *
+ * Copyright (C) 2021  Alviro Iskandar Setiawan <alviro.iskandar@gmail.com>
+ */
+
+#ifndef TGVISD__LOGGER__SENDERFOUNDATION_HPP
+#define TGVISD__LOGGER__SENDERFOUNDATION_HPP
+
+#include <tgvisd/KWorker.hpp>
+
+namespace tgvisd::Logger {
+
+class SenderFoundation
+{
+protected:
+	KWorker				*kworker_ = nullptr;
+	const td_api::MessageSender	&sender_;
+
+	inline SenderFoundation(KWorker *kworker,
+				const td_api::MessageSender &sender):
+		kworker_(kworker),
+		sender_(sender)
+	{
+	}
+};
+
+} /* namespace tgvisd::Logger */
+
+#endif /* #ifndef TGVISD__LOGGER__SENDERFOUNDATION_HPP */
