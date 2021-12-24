c42e51d6d605956917bd4ffcecfce883e2ec51f0
Fandi Muhammad Fajar <fandyesta@gmail.com>
2021-12-24 22:26:05 +0700

    web: Add welcome page in dashboard
    Signed-off-by: Fandi Muhammad Fajar <fandyesta@gmail.com>

--- web/green-tea/components/Dashboard.js
+++ web/green-tea/components/Dashboard.js
@@ -2,7 +2,14 @@ function Dashboard() {
     return (
         <div className="flex-grow text-center">
             <img className="w-[450px] mx-auto" src="greentea.svg" alt="Logo" />
+            <div>
+
+                <h1 className="text-[50px] font-serif" >Welcome Admin</h1>
+                <img className="content-center m-auto"  src="avatar.png"/>
+
+            </div>
         </div>
+        
     );
 }
 
