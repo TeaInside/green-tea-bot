TeaInside/green-tea-bot

commit 41586e2a5145b564a9d8992ab1214ae80cafbed9

greentea/tgvisd: Message: Hold `@chat_lock_` before resolving PK
Since `getPK()` on `Chat` entity classes may perform INSERT and SELECT
to/from database in a non-atomic way, we have to hold the `@chat_lock_`
first.

Signed-off-by: Alviro Iskandar Setiawan <alviro.iskandar@gmail.com>
Link: https://t.me/TeaInside/47420
Signed-off-by: Ammar Faizi <ammarfaizi2@gmail.com>

greentea/tgvisd/Logger/Message.cpp
@@ -95,13 +95,28 @@ bool Message::resolve_sender(void)
 }
 
 bool Message::resolve_pk(void)
+	__acquires(chat_lock_)
+	__releases(chat_lock_)
 {
+	bool ret = false;
 	assert(m_chat_);
 	assert(m_sender_);
+	assert(chat_lock_);
 
-	// pk_chat_id_ = m_chat_->getPK();
-	// pk_sender_id_ = m_sender_->getPK();
-	return true;
+	chat_lock_->lock();
+
+	pk_chat_id_ = m_chat_->getPK();
+	if (unlikely(!pk_chat_id_))
+		goto out;
+
+	pk_sender_id_ = m_sender_->getPK();
+	if (unlikely(!pk_sender_id_))
+		goto out;
+
+	ret = true;
+out:
+	chat_lock_->unlock();
+	return ret;
 }
 
 void Message::save(void)
@@ -114,9 +129,6 @@ void Message::save(void)
 
 	if (!resolve_pk())
 		return;
-
-	chat_lock_->lock();
-	chat_lock_->unlock();
 }
 
 } /* namespace tgvisd::Logger */

commit cb75e39e2157c43f2ec6655e003cd32c2e9ad2da

greentea/tgvisd: Logger: Fix sender creation data race
After commit eebe28fb18798 ("greentea/tgvisd: Logger: Initiate user
logging"), I noticed that we have duplicate user records on the
`gt_users` table.

After several minutes pondering it, I figured out the problem. This
bug was introduced in commit 41586e2a514 ("greentea/tgvisd: Message:
Hold `@chat_lock_` before resolving PK").

Note that we only hold a mutex that we grabbed from `getChatLock()`.
In this case, we do not have any locking with respect to sender info.

Possible data race scenario:
```
       CPU0                      CPU1
   get_sender_pk()          get_sender_pk()
    -> no record              -> no record
   create_sender()          create_sender()
```
Note that `create_sender()` for the same sender can be called twice.
That's the bug here. Fix this by holding `@sender_lock_` before
calling `get_sender_pk()`.

After fixed, no possible data race on sender creation:
```
       CPU0                      CPU1
   sender_lock_->lock()     sender_lock_->lock()
   -- lock acquired --      -- waiting --
   get_sender_pk()          -- waiting --
    -> no record            -- waiting --
   create_sender()          -- waiting --
   sender_lock_->unlock()   -- waiting --
                            -- lock acquired --
                            get_sender_pk()
                              -> get existing PK
                            sender_lock_->unlock()
```

Cc: Iskandar S (Telegram) <@Viro_SSFS>
Cc: Alviro Iskandar Setiawan <alviro.iskandar@gmail.com>
Fixes: 41586e2a5145b56 ("greentea/tgvisd: Message: Hold `@chat_lock_` before resolving PK")
Signed-off-by: Ammar Faizi <ammarfaizi2@gmail.com>

greentea/tgvisd/Logger/Message.cpp
@@ -41,7 +41,7 @@ bool Message::resolve_chat(void)
 	if (!chat_) {
 		chat_ = kworker_->getChat(message_.chat_id_);
 		if (unlikely(!chat_)) {
-			pr_err("get_chat(): "
+			pr_err("resolve_chat(): "
 			       "Could not get chat from message object %ld",
 			       message_.id_);
 			return false;
@@ -51,7 +51,7 @@ bool Message::resolve_chat(void)
 	if (!chat_lock_) {
 		chat_lock_ = kworker_->getChatLock(chat_->id_);
 		if (unlikely(!chat_lock_)) {
-			pr_err("get_chat(): "
+			pr_err("resolve_chat(): "
 			       "Could not get chat lock (%ld) [%s]",
 			       chat_->id_, chat_->title_.c_str());
 			return false;
@@ -77,17 +77,32 @@ bool Message::resolve_chat(void)
 
 bool Message::resolve_sender(void)
 {
+	int64_t lock_id;
 	const auto &s = message_.sender_;
 
 	switch (s->get_id()) {
 	case td_api::messageSenderUser::ID:
 		m_sender_ = new SenderUser(kworker_, *s);
+		if (sender_lock_)
+			break;
+		lock_id = static_cast<const td_api::messageSenderChat &>(*s).chat_id_;
+		sender_lock_ = kworker_->getUserLock(lock_id);
 		break;
 	case td_api::messageSenderChat::ID:
 		m_sender_ = new SenderChat(kworker_, *s);
+		if (sender_lock_)
+			break;
+		lock_id = static_cast<const td_api::messageSenderChat &>(*s).chat_id_;
+		sender_lock_ = kworker_->getChatLock(lock_id);
 		break;
 	default:
 		pr_err("Invalid sender type on resolve_sender()");
+		return false;
+	}
+
+	if (unlikely(!sender_lock_)) {
+		pr_err("resolve_sender(): Could not get chat lock (%ld) [%s]",
+		       chat_->id_, chat_->title_.c_str());
 		return false;
 	}
 
@@ -111,25 +126,28 @@ bool Message::resolve_pk(void)
 	__acquires(chat_lock_)
 	__releases(chat_lock_)
 {
-	bool ret = false;
 	assert(m_chat_);
 	assert(m_sender_);
 	assert(chat_lock_);
+	assert(sender_lock_);
 
 	chat_lock_->lock();
-
 	pk_chat_id_ = m_chat_->getPK();
-	if (unlikely(!pk_chat_id_))
-		goto out;
-
-	pk_sender_id_ = m_sender_->getPK();
-	if (unlikely(!pk_sender_id_))
-		goto out;
-
-	ret = true;
-out:
+	if (unlikely(!pk_chat_id_)) {
+		chat_lock_->unlock();
+		return false;
+	}
 	chat_lock_->unlock();
-	return ret;
+
+	sender_lock_->lock();
+	pk_sender_id_ = m_sender_->getPK();
+	if (unlikely(!pk_sender_id_)) {
+		sender_lock_->unlock();
+		return false;
+	}
+	sender_lock_->unlock();
+
+	return true;
 }
 
 void Message::save(void)

greentea/tgvisd/Logger/Message.hpp
@@ -46,6 +46,7 @@ protected:
 	KWorker					*kworker_ = nullptr;
 	td_api::object_ptr<td_api::chat>	chat_ = nullptr;
 	std::mutex				*chat_lock_ = nullptr;
+	std::mutex				*sender_lock_ = nullptr;
 	mysql::MySQL				*db_ = nullptr;
 
 	/* Models */