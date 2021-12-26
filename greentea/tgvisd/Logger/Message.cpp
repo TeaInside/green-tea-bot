TeaInside/green-tea-bot

commit 8f44e38cf1da939108aad3c79116353c632d7e14

greentea/tgvisd: Move `getDbPool()` call to Message class
This commit fixes a broken logging design that was introduced in commit
8d4025f4951304fb2 ("greentea/tgvisd: Logger: Add getDbPool in foundation
classes").

Do not call `getDbPool()` (instance of `KWorker`) in the ChatFoundation
or SenderFoundation class. Because their destructor will put the DB
pool and we do not actually need two DB pools for this operation.

Consider the following:
```
    pk_chat_id_ = m_chat_->getPK();
    if (unlikely(!pk_chat_id_))
        goto out;

    pk_sender_id_ = m_sender_->getPK();
    if (unlikely(!pk_sender_id_))
        goto out;
```
After those two `getPK()` calls, as long as `m_chat_` and `m_sender_`
still live, they will be holding DB pool instance individually, each
needs to be put on destruction.

That is only the partial craziness, we still have another problem
here. Recall that we still need a DB pool again in the Message class
to save the message, thus we will need to at least hold 3 DB pools
just to save a single message (including chat and sender data), which
clearly does not make any sense!

Why are we wasting so many DB pools just for this small operation!?

So what is the fix for this?
We can heavily reuse this DB pool from the Message class, now we take
the DB pool from KWorker in there, then pass it to ChatFoundation and
SenderFoundation. End result, we only need 1 DB pool to save a single
message.

Cc: Iskandar S (Telegram) <@Viro_SSFS>
Cc: Alviro Iskandar Setiawan <alviro.iskandar@gmail.com>
Fixes: 8d4025f4951304fb2 ("greentea/tgvisd: Logger: Add getDbPool in foundation classes")
Signed-off-by: Ammar Faizi <ammarfaizi2@gmail.com>

greentea/tgvisd/Logger/Message.cpp
@@ -94,6 +94,19 @@ bool Message::resolve_sender(void)
 	return true;
 }
 
+bool Message::resolve_db_pool(void)
+{
+	db_ = kworker_->getDbPool();
+	if (unlikely(!db_))
+		return false;
+
+	assert(m_chat_);
+	assert(m_sender_);
+	m_chat_->setDbPool(db_);
+	m_sender_->setDbPool(db_);
+	return true;
+}
+
 bool Message::resolve_pk(void)
 	__acquires(chat_lock_)
 	__releases(chat_lock_)
@@ -125,6 +138,9 @@ void Message::save(void)
 		return;
 
 	if (!resolve_chat())
+		return;
+
+	if (!resolve_db_pool())
 		return;
 
 	if (!resolve_pk())

greentea/tgvisd/Logger/ChatFoundation.hpp
@@ -17,13 +17,13 @@ namespace tgvisd::Logger {
 class ChatFoundation
 {
 public:
-	virtual ~ChatFoundation(void)
-	{
-		if (db_)
-			kworker_->putDbPool(db_);
-	}
-
+	virtual ~ChatFoundation(void) = default;
 	virtual uint64_t getPK(void) = 0;
+
+	inline void setDbPool(mysql::MySQL *db)
+	{
+		db_ = db;
+	}
 
 protected:
 	KWorker				*kworker_ = nullptr;
@@ -38,15 +38,6 @@ protected:
 
 	inline mysql::MySQL *getDbPool(void)
 	{
-		if (db_)
-			return db_;
-
-		db_ = kworker_->getDbPool();
-		if (unlikely(!db_)) {
-			pr_err("Cannot get DB pool");
-			return nullptr;
-		}
-
 		return db_;
 	}
 };

greentea/tgvisd/Logger/Message.hpp
@@ -46,6 +46,7 @@ protected:
 	KWorker					*kworker_ = nullptr;
 	td_api::object_ptr<td_api::chat>	chat_ = nullptr;
 	std::mutex				*chat_lock_ = nullptr;
+	mysql::MySQL				*db_ = nullptr;
 
 	/* Models */
 	tgvisd::Logger::ChatFoundation		*m_chat_ = nullptr;
@@ -64,6 +65,11 @@ private:
 	 * This will fill @m_chat_ and @chat_lock_.
 	 */
 	bool resolve_chat(void);
+
+	/**
+	 * This will fill @db_.
+	 */
+	bool resolve_db_pool(void);
 
 	/**
 	 * This will fill @pk_chat_id and pk_sender_id_.

greentea/tgvisd/Logger/SenderFoundation.hpp
@@ -17,13 +17,13 @@ namespace tgvisd::Logger {
 class SenderFoundation
 {
 public:
-	virtual ~SenderFoundation(void)
-	{
-		if (db_)
-			kworker_->putDbPool(db_);
-	}
-
+	virtual ~SenderFoundation(void) = default;
 	virtual uint64_t getPK(void) = 0;
+
+	inline void setDbPool(mysql::MySQL *db)
+	{
+		db_ = db;
+	}
 
 protected:
 	KWorker				*kworker_ = nullptr;
@@ -39,15 +39,6 @@ protected:
 
 	inline mysql::MySQL *getDbPool(void)
 	{
-		if (db_)
-			return db_;
-
-		db_ = kworker_->getDbPool();
-		if (unlikely(!db_)) {
-			pr_err("Cannot get DB pool");
-			return nullptr;
-		}
-
 		return db_;
 	}
 };

commit ae3873f3afb808c576ac83908c51f671fd52ed4b

greentea/tgvisd: Logger: Fix DB pool leak
After `getDbPool()` has been moved to `Message` class, we don't have
any subroutine that puts the DB pool on the object destruction. Fix
it by calling `putDbPool()` in the `Message` class' destructor.

Fixes: 8f44e38cf1da9391 ("greentea/tgvisd: Move `getDbPool()` call to Message class")
Signed-off-by: Alviro Iskandar Setiawan <alviro.iskandar@gmail.com>
Link: https://t.me/TeaInside/47663
Signed-off-by: Ammar Faizi <ammarfaizi2@gmail.com>

greentea/tgvisd/Logger/Message.cpp
@@ -34,6 +34,11 @@ Message::~Message(void)
 		delete m_chat_;
 		m_chat_ = nullptr;
 	}
+
+	if (db_) {
+		kworker_->putDbPool(db_);
+		db_ = nullptr;
+	}
 }
 
 bool Message::resolve_chat(void)