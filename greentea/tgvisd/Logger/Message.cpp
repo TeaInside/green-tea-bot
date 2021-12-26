580a455b8c52791da00633488f51e79cce1df21f
Alviro Iskandar Setiawan <alviro.iskandar@gmail.com>
2021-12-27 03:06:01 +0700

    greentea/tgvisd: Logger: Add message content logger
    This adds message content logger, here we track whether the message
    is edited or not, save the text and text entities.
    
    Signed-off-by: Alviro Iskandar Setiawan <alviro.iskandar@gmail.com>
    Link: https://t.me/TeaInside/47671
    Signed-off-by: Ammar Faizi <ammarfaizi2@gmail.com>

--- greentea/tgvisd/Logger/Message.cpp
+++ greentea/tgvisd/Logger/Message.cpp
@@ -7,6 +7,7 @@
  * Copyright (C) 2021  Alviro Iskandar Setiawan <alviro.iskandar@gmail.com>
  */
 
+#include <time.h>
 #include <inttypes.h>
 #include <tgvisd/mysql_helpers.hpp>
 #include <tgvisd/Logger/Message.hpp>
@@ -17,6 +18,11 @@ using ChatGroup = tgvisd::Logger::Chat::Group;
 using ChatUser = tgvisd::Logger::Chat::User;
 
 namespace tgvisd::Logger {
+
+static inline void *bn_str(bool p)
+{
+	return (void *) (p ? "1" : "0");
+}
 
 Message::Message(KWorker *kworker, const td_api::message &message):
 	message_(message),
@@ -164,6 +170,16 @@ static uint64_t save_message_if_not_exist(mysql::MySQL *db,
 
 void Message::save(void)
 {
+	if (unlikely(!message_.content_))
+		return;
+
+	/*
+	 * Currently, we only save text message.
+	 * TODO: Handle other types of message, like photo, sticker, etc.
+	 */
+	if (message_.content_->get_id() != td_api::messageText::ID)
+		return;
+
 	if (!resolve_sender())
 		return;
 
@@ -179,6 +195,113 @@ void Message::save(void)
 	chat_lock_->lock();
 	save_message_if_not_exist(db_, message_, pk_chat_id_, pk_sender_id_);
 	chat_lock_->unlock();
+}
+
+static size_t convert_epoch_to_db_format(char *buf, size_t buf_size,
+					 time_t time)
+{
+	struct tm tm;
+	memset(&tm, 0, sizeof(struct tm));
+	gmtime_r(&time, &tm);
+	return strftime(buf, buf_size, "%Y-%m-%d %H:%M:%S", &tm);
+}
+
+static uint64_t create_message_content(mysql::MySQL *db,
+				       const td_api::message &message,
+				       uint64_t pk_message_id)
+{
+	char tg_date[64];
+	size_t tg_date_size;
+	time_t tg_date_epoch;
+	std::string entities_txt;
+	uint64_t pk_message_content_id;
+	mysql::MySQLStmt *stmt = nullptr;
+	const char *stmtErrFunc = nullptr;
+
+	const auto &content = static_cast<const td_api::messageText &>(*message.content_);
+	if (unlikely(!content.text_))
+		return 0;
+
+	const auto &formattedText = *content.text_;
+	const auto &text = formattedText.text_;
+	const auto &entities = formattedText.entities_;
+
+	stmt = db->prepare(5,
+		"INSERT INTO `gt_message_content` "
+		"("
+			"`message_id`,"
+			"`text`,"
+			"`text_entities`,"
+			"`is_edited_msg`,"
+			"`tg_date`,"
+			"`created_at`"
+		")"
+			" VALUES "
+		"("
+			"?,"	/* messsage_id */
+			"?,"	/* text */
+			"?,"	/* text_entities */
+			"?,"	/* is_edited_msg */
+			"?,"	/* tg_date */
+			"NOW()"	/* created_at */
+		");"
+	);
+
+	if (MYSQL_IS_ERR_OR_NULL<mysql::MySQLStmt>(stmt)) {
+		mysql_handle_prepare_err(db, stmt);
+		return 0;
+	}
+
+	if (unlikely(stmt->stmtInit())) {
+		stmtErrFunc = "stmtInit";
+		goto stmt_err;
+	}
+
+	stmt->bind(0, MYSQL_TYPE_LONGLONG, (void *) &pk_message_id,
+		   sizeof(pk_message_id));
+	stmt->bind(1, MYSQL_TYPE_STRING, (void *) text.c_str(), text.size());
+
+	if (!entities.size()) {
+		stmt->bind(2, MYSQL_TYPE_NULL, NULL, 0);
+	} else {
+		entities_txt = to_string(entities);
+		void *p = (void *) entities_txt.c_str();
+		stmt->bind(2, MYSQL_TYPE_STRING, p, entities_txt.size());
+	}
+
+	stmt->bind(3, MYSQL_TYPE_STRING, bn_str(!!message.edit_date_), 1);
+
+	if (message.edit_date_)
+		tg_date_epoch = message.edit_date_;
+	else
+		tg_date_epoch = message.date_;
+
+	tg_date_size = convert_epoch_to_db_format(tg_date, sizeof(tg_date),
+						  tg_date_epoch);
+
+	if (tg_date_size)
+		stmt->bind(4, MYSQL_TYPE_STRING, tg_date, tg_date_size);
+	else
+		stmt->bind(4, MYSQL_TYPE_NULL, NULL, 0);
+
+	if (unlikely(stmt->bindStmt())) {
+		stmtErrFunc = "bindStmt";
+		goto stmt_err;
+	}
+
+	if (unlikely(stmt->execute())) {
+		stmtErrFunc = "execute";
+		goto stmt_err;
+	}
+
+	pk_message_content_id = stmt->getInsertId();
+	goto out;
+
+stmt_err:
+	mysql_handle_stmt_err(stmtErrFunc, stmt);
+	pk_message_content_id = 0;
+out:
+	return pk_message_content_id;
 }
 
 static uint64_t create_message(mysql::MySQL *db, const td_api::message &message,
@@ -215,7 +338,7 @@ static uint64_t create_message(mysql::MySQL *db, const td_api::message &message,
 			"?,"		/* is_forwarded_msg */
 			"?,"		/* is_deleted */
 			"NOW(),"	/* created_at */
-			"NULL"
+			"NULL"		/* updated_at */
 		");"
 	);
 
@@ -263,6 +386,9 @@ static uint64_t create_message(mysql::MySQL *db, const td_api::message &message,
 	}
 
 	pk_message_id = stmt->getInsertId();
+
+	if (unlikely(!create_message_content(db, message, pk_message_id)))
+		pk_message_id = 0;
 	goto out;
 
 stmt_err:
