e5a9cb41dfcf1b784a174f58f769d965bc95e083
Alviro Iskandar Setiawan <alviro.iskandar@gmail.com>
2021-12-27 02:26:22 +0700

    greentea/tgvisd: Logger: Implement message SQL log query
    This adds SQL queries to save message and get the PK of the
    corresponding message from the database.
    
    Signed-off-by: Alviro Iskandar Setiawan <alviro.iskandar@gmail.com>
    Link: https://t.me/TeaInside/47670
    Signed-off-by: Ammar Faizi <ammarfaizi2@gmail.com>

--- greentea/tgvisd/Logger/Message.cpp
+++ greentea/tgvisd/Logger/Message.cpp
@@ -7,6 +7,8 @@
  * Copyright (C) 2021  Alviro Iskandar Setiawan <alviro.iskandar@gmail.com>
  */
 
+#include <inttypes.h>
+#include <tgvisd/mysql_helpers.hpp>
 #include <tgvisd/Logger/Message.hpp>
 
 using SenderUser = tgvisd::Logger::Sender::User;
@@ -155,6 +157,11 @@ bool Message::resolve_pk(void)
 	return true;
 }
 
+static uint64_t save_message_if_not_exist(mysql::MySQL *db,
+					  const td_api::message &message,
+					  uint64_t pk_chat_id,
+					  uint64_t pk_sender_id);
+
 void Message::save(void)
 {
 	if (!resolve_sender())
@@ -168,6 +175,179 @@ void Message::save(void)
 
 	if (!resolve_pk())
 		return;
+
+	chat_lock_->lock();
+	save_message_if_not_exist(db_, message_, pk_chat_id_, pk_sender_id_);
+	chat_lock_->unlock();
+}
+
+static uint64_t create_message(mysql::MySQL *db, const td_api::message &message,
+			       uint64_t pk_chat_id, uint64_t pk_sender_id)
+{
+	uint64_t tg_msg_id;
+	uint64_t pk_message_id;
+	uint64_t reply_to_tg_msg_id;
+	mysql::MySQLStmt *stmt = nullptr;
+	const char *stmtErrFunc = nullptr;
+
+	stmt = db->prepare(8,
+		"INSERT INTO `gt_messages` "
+		"("
+			"`chat_id`,"
+			"`sender_id`,"
+			"`tg_msg_id`,"
+			"`reply_to_tg_msg_id`,"
+			"`msg_type`,"
+			"`has_edited_msg`,"
+			"`is_forwarded_msg`,"
+			"`is_deleted`,"
+			"`created_at`,"
+			"`updated_at`"
+		")"
+			" VALUES "
+		"("
+			"?,"		/* chat_id */
+			"?,"		/* sender_id */
+			"?,"		/* tg_msg_id */
+			"?,"		/* reply_to_tg_msg_id */
+			"?,"		/* msg_type */
+			"?,"		/* has_edited_msg */
+			"?,"		/* is_forwarded_msg */
+			"?,"		/* is_deleted */
+			"NOW(),"	/* created_at */
+			"NULL"
+		");"
+	);
+
+	if (MYSQL_IS_ERR_OR_NULL<mysql::MySQLStmt>(stmt)) {
+		mysql_handle_prepare_err(db, stmt);
+		return 0;
+	}
+
+	if (unlikely(stmt->stmtInit())) {
+		stmtErrFunc = "stmtInit";
+		goto stmt_err;
+	}
+
+	tg_msg_id = message.id_ >> 20u;
+	reply_to_tg_msg_id = message.reply_to_message_id_ >> 20u;
+
+	stmt->bind(0, MYSQL_TYPE_LONGLONG, (void *) &pk_chat_id,
+		   sizeof(pk_chat_id));
+	stmt->bind(1, MYSQL_TYPE_LONGLONG, (void *) &pk_sender_id,
+		   sizeof(pk_sender_id));
+	stmt->bind(2, MYSQL_TYPE_LONGLONG, (void *) &tg_msg_id,
+		   sizeof(tg_msg_id));
+
+	if (reply_to_tg_msg_id)
+		stmt->bind(3, MYSQL_TYPE_LONGLONG, (void *) &reply_to_tg_msg_id,
+			   sizeof(reply_to_tg_msg_id));
+	else
+		stmt->bind(3, MYSQL_TYPE_NULL, NULL, 0);
+
+	stmt->bind(4, MYSQL_TYPE_STRING, (void *) "text", 4);
+	stmt->bind(5, MYSQL_TYPE_STRING,
+		   (void *) (message.edit_date_ ? "1" : "0"), 1);
+	stmt->bind(6, MYSQL_TYPE_STRING,
+		   (void *) (message.forward_info_ ? "1" : "0"), 1);
+	stmt->bind(7, MYSQL_TYPE_STRING, (void *) "0" , 1);
+
+	if (unlikely(stmt->bindStmt())) {
+		stmtErrFunc = "bindStmt";
+		goto stmt_err;
+	}
+
+	if (unlikely(stmt->execute())) {
+		stmtErrFunc = "execute";
+		goto stmt_err;
+	}
+
+	pk_message_id = stmt->getInsertId();
+	goto out;
+
+stmt_err:
+	mysql_handle_stmt_err(stmtErrFunc, stmt);
+	pk_message_id = 0;
+out:
+	delete stmt;
+	return pk_message_id;
+}
+
+static uint64_t get_message_pk(mysql::MySQL *db, const td_api::message &message,
+			       uint64_t pk_chat_id, uint64_t pk_sender_id)
+{
+	static const char q[] =
+		"SELECT id FROM gt_messages WHERE "
+		"chat_id = %" PRIu64 " AND tg_msg_id = %" PRIu64;
+
+	int qlen, tmp;
+	MYSQL_ROW row;
+	mysql::MySQLRes *res;
+	uint64_t tg_msg_id;
+	uint64_t pk_message_id;
+	char qbuf[sizeof(q) + 128];
+
+	tg_msg_id = message.id_ >> 20u;
+	qlen = snprintf(qbuf, sizeof(qbuf), q, pk_chat_id, tg_msg_id);
+
+	tmp = db->realQuery(qbuf, (size_t) qlen);
+	if (unlikely(tmp)) {
+		pr_err("query(): %s", db->getError());
+		return 0;
+	}
+
+	res = db->storeResult();
+	if (MYSQL_IS_ERR_OR_NULL(res)) {
+		pr_err("storeResult(): %s", db->getError());
+		return 0;
+	}
+
+	row = res->fetchRow();
+	if (!row) {
+		pk_message_id = create_message(db, message, pk_chat_id,
+					       pk_sender_id);
+		goto out;
+	}
+
+	pk_message_id = strtoull(row[0], NULL, 10);
+out:
+	delete res;
+	return pk_message_id;
+}
+
+static uint64_t save_message_if_not_exist(mysql::MySQL *db,
+					  const td_api::message &message,
+					  uint64_t pk_chat_id,
+					  uint64_t pk_sender_id)
+{
+	int tmp;
+	uint64_t pk_message_id;
+
+	tmp = db->beginTransaction();
+	if (unlikely(tmp)) {
+		pr_err("beginTransaction(): %s", db->getError());
+		return 0;
+	}
+
+	pk_message_id = get_message_pk(db, message, pk_chat_id, pk_sender_id);
+	if (unlikely(!pk_message_id))
+		goto rollback;
+
+	tmp = db->commit();
+	if (unlikely(tmp)) {
+		pr_err("commit(): %s", db->getError());
+		goto rollback;
+	}
+
+	/* Success! */
+	pr_notice("pk_message_id = %" PRIu64, pk_message_id);
+	return pk_message_id;
+
+rollback:
+	tmp = db->rollback();
+	if (unlikely(tmp))
+		pr_err("rollback(): %s", db->getError());
+	return 0;
 }
 
 } /* namespace tgvisd::Logger */
