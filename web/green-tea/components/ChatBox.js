04a1e0128f8910cb808ba7a11055bf1eb3c83a10
Ammar Faizi <ammarfaizi2@gmail.com>
2022-01-04 16:53:26 +0700

    web: Fix chat log page
    This fixes the chat log page. I use react component for this view.
    Added a new component named ChatContainer, and moved the ChatBox and
    GroupList into ChatContainer's render() method.
    
    In the ChatContainer, we also handle the API fetch and data caching.
    
    Signed-off-by: Ammar Faizi <ammarfaizi2@gmail.com>

--- web/green-tea/components/ChatBox.js
+++ web/green-tea/components/ChatBox.js
@@ -6,7 +6,7 @@ import Message from "./Message";
 function ChatBox({ data }) {
     const [offcanvas, setOffcanvas] = useState(false);
     return (
-        <div className="flex-grow flex  h-screen bg-cream">
+        <div className="flex-grow flex h-screen bg-cream">
             <div className={classNames("transition-all duration-150 ease-in-out", offcanvas ? "w-8/12" : "w-full")}>
                 <div className="flex items-center bg-white w-full pl-5 h-[80px] shadow-md sticky top-0">
                     <img className="w-12 h-12 rounded-full" src="profile.jpeg" alt="" />
@@ -17,18 +17,9 @@ function ChatBox({ data }) {
                     <SearchIcon className="w-8 h-8 ml-auto mr-4 text-gray-400 cursor-pointer" onClick={() => setOffcanvas(true)} />
                 </div>
                 <div className="space-y-6 flex flex-col  overflow-y-scroll h-[89%] whitespace-nowrap p-4 scrollbar-thin scrollbar-thumb-gray-400">
-                    {data.map(({ first_name, last_name, text }) => (
-                        <Message first_name={first_name} last_name={last_name} text={text} />
+                    {data.map(({ id, first_name, last_name, text }) => (
+                        <Message key={id} first_name={first_name} last_name={last_name} text={text} />
                     ))}
-
-                    {/* <Message />
-                    <Message />
-                    <Message />
-                    <Message />
-                    <Message />
-                    <Message />
-                    <Message />
-                    <Message /> */}
                 </div>
             </div>
             <div className={classNames("w-4/12 bg-white", offcanvas ? "block" : "hidden")}>
--- web/green-tea/components/Group.js
+++ web/green-tea/components/Group.js
@@ -1,13 +1,24 @@
-function Group({ groupName, groupId, lastMessage }) {
-    return (
-        <div className="flex items-center cursor-pointer p-2 hover:bg-gray-200">
-            <img className="w-12 h-12 rounded-full" src="profile.jpeg" alt="" />
-            <div className="ml-4">
-                <h2>{"(" + groupId + ") " + "" + groupName}</h2>
-                <p className="text-gray-400">{lastMessage}</p>
+import React, { useState } from 'react';
+
+class Group extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div
+                onClick={(e) => this.props.container.fetchChatBoxData(this.props.groupId)}
+                className="flex items-center cursor-pointer p-2 hover:bg-gray-200"
+            >
+                <img className="w-12 h-12 rounded-full" src="profile.jpeg" alt=""/>
+                <div className="ml-4">
+                    <h2>{"(" + this.props.groupId + ") " + "" + this.props.groupName}</h2>
+                    <p className="text-gray-400">{this.props.lastMessage}</p>
+                </div>
             </div>
-        </div>
-    );
-}
+        );
+    }
+};
 
 export default Group;
