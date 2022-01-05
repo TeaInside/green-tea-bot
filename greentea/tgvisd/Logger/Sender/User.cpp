TeaInside/green-tea-bot

commit b83eb63f41dc4a9c6fe885c5c1cc00d60a397663

greentea/tgvisd: Logger: Fix wrong first_name logger bug
There is a possibility that the first_name can be null, it is when
the log hits a deleted account. Make sure we handle this case
properly instead of logging an empty string.

Signed-off-by: Alviro Iskandar Setiawan <alviro.iskandar@gmail.com>
Link: https://t.me/TeaInside/47675
Signed-off-by: Ammar Faizi <ammarfaizi2@gmail.com>

greentea/tgvisd/Logger/Sender/User.cpp
@@ -96,7 +96,7 @@ static uint64_t create_user_history(mysql::MySQL *db, struct user_data *ud,
 			   user.username_.size());
 
 	if (!user.first_name_.size())
-		stmt->bind(2, MYSQL_TYPE_STRING, (void *) "", 0);
+		stmt->bind(2, MYSQL_TYPE_NULL, NULL, 0);
 	else
 		stmt->bind(2, MYSQL_TYPE_STRING,
 			   (void *) user.first_name_.c_str(),

commit 60e60f7eb32fdb6dcd92c62b4c4f504ab15eb552

greentea/tgvisd: Logger: Fix wrong first_name logger bug (2)
This is a fix for the fix (yeah, /facepalm).

Commit b83eb63f41dc4a9c6fe885c5c1cc00d60a397663 ("greentea/tgvisd:
Logger: Fix wrong first_name logger bug") did not properly fix the
first_name empty string bug when saving a deleted account info.

I found that the first_name on table gt_users for the deleted account
is still an empty string. It should be NULL.

JFYI, the current handler properly handles the `first_name` on table
`gt_users_history`, so you don't have to worry about checking that
part.

Fix this by providing the correct bind param type.

A short technical story about this saving:
AFAICT, deleted account always doesn't have a `first_name` and
`last_name`. So it is perfectly safe to assume those values are
always NULL only for a deleted account. That is why for history
tracking purposes, we also save the account type in the history
table, because the account type may change (e.g. from "user" to
"deleted").

When the account gets deleted, we may still have the old history
of this user (including original `username`, `first_name`, and
`last_name`).

Telegram-ping: Iskandar S (@Viro_SSFS)
Cc: Alviro Iskandar Setiawan <alviro.iskandar@gmail.com>
Fixes: b83eb63f41dc4a9c ("greentea/tgvisd: Logger: Fix wrong first_name logger bug")
Signed-off-by: Ammar Faizi <ammarfaizi2@gmail.com>

greentea/tgvisd/Logger/Sender/User.cpp
@@ -232,7 +232,7 @@ static uint64_t create_user(mysql::MySQL *db, struct user_data *ud)
 			   user.username_.size());
 
 	if (!user.first_name_.size())
-		stmt->bind(2, MYSQL_TYPE_STRING, (void *) "", 0);
+		stmt->bind(2, MYSQL_TYPE_NULL, NULL, 0);
 	else
 		stmt->bind(2, MYSQL_TYPE_STRING,
 			   (void *) user.first_name_.c_str(),