13fdb13bd09ba8095caca1e6a3d870c39a9fabb9
Ilham Muttaqien <ilham.muttaqien@students.amikom.ac.id>
2022-01-07 22:12:13 +0700

    web: Edit UI statistic page
    Signed-off-by: Ilham Muttaqien <ilham.muttaqien@students.amikom.ac.id>

--- web/green-tea/components/StatisticContent.js
+++ web/green-tea/components/StatisticContent.js
@@ -1,16 +1,15 @@
-import GraphStatistic from "./GraphStatistic"
-import TableStatistic from "./TableStatistic"
+import GraphStatistic from "./GraphStatistic";
+import TableStatistic from "./TableStatistic";
 
-export default function StatisticContent(){
+export default function StatisticContent() {
     return (
-        <div className="w-full h-screen bg-blue-800">
-            <h1 className="text-center text-[6em]">STATISTIC</h1>
+        <div className="w-full h-screen bg-cream overflow-y-scroll whitespace-nowrap py-10">
+            <h1 className="text-center text-[3em]">STATISTIC</h1>
 
-            <div className="grid grid-cols-3 grid-rows-2 h-[800px] mx-16 mt- gap-x-10 mb-10">
+            <div className="grid grid-cols-3 grid-rows-2 mx-16 mt-8 gap-x-10">
                 <GraphStatistic />
                 <TableStatistic />
             </div>
         </div>
-        
-    )
+    );
 }
